3206591a10fddc211a0be0eb686df70ce6d3d331
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-09-15 20:11:20 +0200

    Added first implementation of stem base
    seems attribute exploration is not far away from now...
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- conexp/fca/implications.clj
+++ conexp/fca/implications.clj
@@ -5,7 +5,8 @@
    :init init
    :constructors { [ clojure.lang.PersistentHashSet clojure.lang.PersistentHashSet ] [] }
    :state state)
-  (:use conexp.util
+  (:use clojure.set
+	conexp.util
 	conexp.base
 	conexp.fca.contexts))
 
@@ -35,3 +36,66 @@
 (defn holds? [impl ctx]
   (forall [intent (context-intents ctx)]
 	  (respects? intent impl)))
+
+(defn add-immediate-elements
+  ([implications set]
+     (add-immediate-elements implications set #{}))
+
+  ([implications new-set old-set]
+     (if (= new-set old-set)
+       new-set
+       (let [conclusions-to-add (for [impl implications
+				      :when (and (subset? (premise impl) new-set)
+						 (exists [x (premise impl)] (not (old-set x))))]
+				  (conclusion impl))]
+	 (apply union new-set conclusions-to-add)))))
+
+(defn close-under-implications [implications set]
+  (first (first (drop-while (fn [[old new]] (not= old new))
+			    (iterate (fn [[old new]]
+				       [new (add-immediate-elements implications new old)])
+				     [#{} set])))))
+
+(defn clop-by-implications [implications]
+  (partial close-under-implications implications))
+
+(defn follows-semantically? [implication implications]
+  (subset? (conclusion implication) (close-under-implications implications (premise implication))))
+
+;;; copied, thus needs a good idea for refactoring
+(defn add-immediate-elements*
+  ([implications set]
+     (add-immediate-elements* implications set #{}))
+
+  ([implications new-set old-set]
+     (if (= new-set old-set)
+       new-set
+       (let [conclusions-to-add (for [impl implications
+				      :when (and (proper-subset? (premise impl) new-set)
+						 (exists [x (premise impl)] (not (old-set x))))]
+				  (conclusion impl))]
+	 (apply union new-set conclusions-to-add)))))
+
+(defn close-under-implications* [implications set]
+  (first (first (drop-while (fn [[old new]] (not= old new))
+			    (iterate (fn [[old new]]
+				       [new (add-immediate-elements* implications new old)])
+				     [#{} set])))))
+
+(defn clop-by-implications* [implications]
+  (partial close-under-implications* implications))
+
+(defn stem-base [ctx]
+  (let [double-prime (partial context-attribute-closure ctx)
+	attributes   (attributes ctx)]
+    (loop [implications #{}
+	   last         #{}]
+      (let [conclusion-from-last (double-prime last)
+	    implications (if (not= last conclusion-from-last)
+			   (conj implications (make-implication last conclusion-from-last))
+			   implications)
+	    clop (clop-by-implications* implications)
+	    next (next-closed-set attributes clop last)]
+	(if next
+	  (recur implications next)
+	  implications)))))
