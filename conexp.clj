5a37b275bbe0834710db3fe30c34ac5b8414bb44
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-09-29 11:56:59 +0200

    Added compile-conexp to conexp.clj
    to not to have conexp recompiled every time it is used.
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- conexp.clj
+++ conexp.clj
@@ -1,17 +1,17 @@
 (ns conexp
   (:use [clojure.contrib.ns-utils :only (immigrate)]))
 
+(defn compile-conexp []
+  (compile 'conexp.fca.contexts)
+  (compile 'conexp.fca.implications)
+  (compile 'conexp.fca.lattices)
+  (compile 'conexp.gui.repl))
+
+(compile-conexp)
+
 ; Use immigrate, so that all imports behave as if they were defined in here.
 ; Thus (use 'conexp) makes sense.
 
-(immigrate 'conexp.base)
-
-(compile 'conexp.fca.contexts)
-(compile 'conexp.fca.implications)
-(compile 'conexp.fca.lattices)
-
-(immigrate 'conexp.fca)
-
-(compile 'conexp.gui.repl)
-
-(immigrate 'conexp.gui)
+(immigrate 'conexp.base
+	   'conexp.fca
+	   'conexp.gui)
