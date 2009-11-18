1d26f382147a5915d700a9b6ca5d4e2ea65b5db4
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-11-18 16:15:15 +0100

    Scale functions for layouts added.
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/layout/util.clj
+++ src/conexp/layout/util.clj
@@ -1,24 +1,66 @@
 (ns conexp.layout.util
-  (:use conexp.graphics.base
+  (:use conexp.base
+	conexp.graphics.base
 	conexp.fca.lattices
-	clojure.contrib.graph))
+	[clojure.contrib.graph :exclude (transitive-closure)]))
 
-(defn lattice->graph [lattice]
+(defn lattice->graph
+  "Converts given lattice to it's corresponding graph with loops
+  removed."
+  [lattice]
   (remove-loops
    (struct-map directed-graph
      :nodes (base-set lattice)
-     :neighbors (memoize 
+     :neighbors (memoize
 		 (fn [x]
 		   (let [order (order lattice)]
 		     (filter #(order [x %]) (base-set lattice))))))))
 
-(defn layers [lattice]
+(defn layers
+  "Returns the layers of the given lattice, that is sequence of points
+  with equal heights."
+  [lattice]
   (dependency-list (lattice->graph lattice)))
 
 (defn scale-points-to-rectangle
-  "Scales the collection of points such that they fit int the
+  "Scales the collection of points such that they fit in the
   rectangle given by [x1 y1] and [x2 y2]."
   [[x1 y1] [x2 y2] points]
-  'to-be-done)  
+  (if (empty? points)
+    (illegal-argument (str "Cannot scale empty sequence of points.")))
+  (if (or (= x1 x2)
+	  (= y1 y2))
+    (illegal-argument (str "Cannot scale to rectangle with size 0.")))
+  (let [[x0 y0] (first points)
+	[x_min y_min x_max y_max] (loop [x_min x0
+					 y_min y0
+					 x_max x0
+					 y_max y0
+					 points (rest points)]
+				    (if (empty? points)
+				      [x_min y_min x_max y_max]
+				      (let [[x y] (first points)]
+					(recur (min x x_min)
+					       (min y y_min)
+					       (max x x_max)
+					       (max y y_max)
+					       (rest points)))))
+	a_x (/ (- x1 x2) (- x_min x_max))
+	b_x (- x1 (* a_x x_min))
+	a_y (/ (- y1 y2) (- y_min y_max))
+	b_y (- y1 (* a_y y_min))]
+    (map (fn [[x y]]
+	   [(+ (* a_x x) b_x) (+ (* a_y y) b_y)])
+	 points)))
+
+(defn scale-layout
+  "Scales given layout to rectangle (x1 y1), (x2 y2). Layout is given
+  as a map of points to coordinates and a sequence of connection pairs."
+  [[x1 y1] [x2 y2] [points-to-coordinates point-connections]]
+  (let [points (seq points-to-coordinates)]
+    [(apply hash-map (interleave (map first points)
+				 (scale-points-to-rectangle [x1 y1] [x2 y2]
+							    (map second points))))
+     point-connections]))
 
 nil
