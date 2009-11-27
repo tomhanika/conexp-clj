cebed1e937a627b241cac45a8d5d0db09eccd6af
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-11-27 21:29:31 +0100

    Added todo-list
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ src/conexp/todo.clj
@@ -0,0 +1,45 @@
+(ns conexp.todo)
+
+;;
+;; conexp.base
+;;  - make next-closure faster
+;;  - invent and implement parallel-next-closure
+;;
+;; conexp.layout
+;;  - util: implement functions for inf-irr-additive layout
+;;  - force: implement force layout as described by C. Zschalig
+;;  - zschalig: implement initial positioning and force distribution layout as described by C. Zschalig
+;;
+;; conexp.fca
+;;  - retracts?
+;;  - rule-exploration: think about and write some code
+;;  - more for association-rules?
+;;
+;; conexp.graphics
+;;  - draw: implement full lattice editor
+;;  - base: get current lattice layout from a GScene
+;;
+;; conexp.gui
+;;  - plugins: design and implement
+;;  - plugins.contexteditor: design and implement
+;;  - plugins.latticeeditor: design and implement (see conexp.graphics.draw)
+;;  - plugins.codeeditor: design and implement
+;;
+;; conexp.contrib
+;;  - fuzzy: implement some fuzzy fca
+;;  - rough: implement something for rough sets and fca
+;;
+;; conexp.tests
+;;  - util: add more tests
+;;  - base: check whether everything has been tested
+;;  - fca.contexts: finish tests
+;;  - fca.implications: finish tests
+;;  - add tests for everything else too
+;;
+;; doc
+;;  - for namespaces
+;;  - for functions (see conexp.contrib.doc)
+;;  - extend LaTeX-doc
+;;
+
+nil
