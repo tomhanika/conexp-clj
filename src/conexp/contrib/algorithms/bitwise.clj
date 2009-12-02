114a5994c77cfd913ed119bca702693c75c51c7b
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-12-02 19:06:20 +0100

    Added base structure for conexp.contrib.algorithms
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ src/conexp/contrib/algorithms/bitwise.clj
@@ -0,0 +1,15 @@
+(ns #^{:doc "Algorithms which work with bits to improve performance."}
+  conexp.contrib.algorithms.bitwise
+  (:import [java.util BitSet]))
+
+;;; Helpers to convert to and from BitSets
+
+;;; NextClosure with BitSets
+
+;;; Krajca-Outrata-Vychodil
+
+;;; In-Close?
+
+;;;
+
+nil
