5d07f70cec270072263157b10a6dd9ba29f11036
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-12-12 10:00:32 +0100

    Version update
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp.clj
+++ src/conexp.clj
@@ -16,7 +16,7 @@
 
 (def *conexp-version* {:major 0,
 		       :minor 1,
-		       :timestamp 1260608018834,
+		       :timestamp 1260608414460,
 		       :qualifier "pre-alpha"})
 
 (defn conexp-version
