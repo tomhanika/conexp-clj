41d9c776e66e2f6d6a5dab383bb5b17dbe25d606
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2009-12-31 11:39:57 +0100

    First sketch for gui plugins
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ src/conexp/gui/plugins.clj
@@ -0,0 +1,17 @@
+(ns conexp.gui.plugins)
+
+; structure for plugin management, gets a frame to operate on
+
+; (defmacro define-plugin ...)
+
+; (defn register-plugin ...)
+
+; (defn load-plugin ...)
+
+; (defn unload-plugin ...)
+
+; (defn loaded-plugins ...)
+
+; (defn registered-plugins ...)
+
+nil
