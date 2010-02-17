1752aa0407064ae1b25bf5ce2a4bec7ba234612d
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-02-17 09:34:33 +0100

    Added generic testing function to conexp.current
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp.clj
+++ src/conexp.clj
@@ -1,8 +1,8 @@
 (ns conexp
   (:use [clojure.contrib.ns-utils :only (immigrate)]))
 
-; Use immigrate, so that all imports behave as if they were defined in here.
-; Thus (use 'conexp) makes sense.
+;; Use immigrate, so that all imports behave as if they were defined in here.
+;; Thus (use 'conexp) makes sense.
 
 (def *conexp-namespaces* '[conexp.base
 			   conexp.fca
--- src/conexp/current.clj
+++ src/conexp/current.clj
@@ -2,13 +2,23 @@
   (:require conexp
 	    clojure.contrib.pprint
 	    clojure.contrib.repl-utils))
+;;;
 
+(defmulti go
+  "Main testing function."
+  (fn [& args] (first args)))
+
+(defmethod go :default [& args]
+  (conexp/illegal-argument "No tester for " args))
 ;;;
 
 (defn- make-test-lattice
   ""
   [n]
   (conexp/concept-lattice (conexp/rand-context (conexp/set-of-range n) 0.4)))
+
+(defmethod go :draw [_ n]
+  (conexp/draw-lattice (make-test-lattice n)))
 
 ;;;
 
