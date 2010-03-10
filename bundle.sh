d362fc92da8ff702842e7926b6769b575ea39a17
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-03-10 14:47:59 +0100

    Changed projects name to conexp-clj
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bundle.sh
+++ bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p lib && cp src/lib/*.jar lib/ && cp src/lib/*.clj lib && \
-lein deps && lein jar && mv conexp.jar lib && \
+lein deps && lein jar && mv conexp-clj.jar lib && \
 zip -r conexp-clj.zip bin lib AUTHORS LICENSE README
