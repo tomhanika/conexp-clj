95ea1f91ec4a70c0da39befbdf45beb2ba72a650
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-04-28 13:43:52 +0200

    Added skeleton implementation for testing EL-gfp rewriting
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ src/conexp/contrib/tests/dl/languages/EL_gfp_rewriting.clj
@@ -0,0 +1,22 @@
+;; Copyright (c) Daniel Borchmann. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns conexp.contrib.tests.dl.languages.EL-gfp-rewriting
+  (:use conexp.main
+        conexp.contrib.dl.framework.syntax
+        conexp.contrib.dl.framework.semantics
+        conexp.contrib.dl.languages.EL-gfp-rewriting
+        conexp.contrib.tests.dl.examples)
+  (:use clojure.test))
+
+;;;
+
+
+;;;
+
+nil
