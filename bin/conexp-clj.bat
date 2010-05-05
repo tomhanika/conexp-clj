ef93a9db9cf8908580680b5b3588820160e9cbc7
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-05-05 10:30:46 +0200

    Fixed bug in batch files for windows
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bin/conexp-clj.bat
+++ bin/conexp-clj.bat
@@ -3,4 +3,4 @@
 set CONEXP_CLJ_HOME=..
 set CONEXP_CLJ_CONSOLE_INIT=%CONEXP_CLJ_HOME%/lib/conexp-clj-console-init.clj
 
-java -cp %CONEXP_CLJ_HOME%/lib/\* jline.ConsoleRunner clojure.main -i %CONEXP_CLJ_CONSOLE_INIT%
+java -cp "%CONEXP_CLJ_HOME%/lib/*" jline.ConsoleRunner clojure.main -i %CONEXP_CLJ_CONSOLE_INIT% -r
