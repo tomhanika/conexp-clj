78357b35b59cd08a9973dbd4eb29fe7ed04d4135
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-05-24 23:12:05 +0200

    Changed dos batch parameter for "all arguments"
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bin/conexp-clj.bat
+++ bin/conexp-clj.bat
@@ -3,4 +3,4 @@
 set CONEXP_CLJ_HOME=..
 set CONEXP_CLJ_INIT=%CONEXP_CLJ_HOME%/lib/conexp-clj.clj
 
-java -cp "%CONEXP_CLJ_HOME%/lib/*" jline.ConsoleRunner clojure.main -i %CONEXP_CLJ_INIT% -r %1
+java -cp "%CONEXP_CLJ_HOME%/lib/*" jline.ConsoleRunner clojure.main -i %CONEXP_CLJ_INIT% -r %~*
