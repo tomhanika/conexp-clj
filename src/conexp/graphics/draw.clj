6fa3ad0eac6e88d73333610e4feefa561c239f18
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-06-21 20:39:45 +0200

    Factored out buttons of lattice editor
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/graphics/draw.clj
+++ src/conexp/graphics/draw.clj
@@ -42,8 +42,9 @@
 						      all-inf-add-influenced-nodes,
 						      all-sup-add-influenced-nodes,
 						      *default-node-radius*,
-						      set-node-radius!)]
-	clojure.contrib.swing-utils)
+						      set-node-radius!)],
+        conexp.graphics.buttons)
+  (:use clojure.contrib.swing-utils)
   (:import [javax.swing JFrame JPanel JButton JTextField JLabel
 	                JSeparator SwingConstants BoxLayout Box
 	                JScrollBar JComboBox JScrollPane JFileChooser]
@@ -58,10 +59,6 @@
 
 
 ;;; Lattice Editor
-
-(declare make-button, make-label, make-labeled-text-field,
-	 make-padding, make-separator, make-combo-box)
-
 
 ;; editor features
 
@@ -301,75 +298,6 @@
 				  layout (@saved-layouts selected)]
 			      (update-layout-of-scene scn layout)))))
     (save-layout nil)))
-
-
-;;; Buttons, Labels and the like
-
-(defvar- *item-width* 100
-  "Width of items in toolbar.")
-
-(defvar- *item-height* 25
-  "Heights of items on toolbar.")
-
-(defvar- *toolbar-width* (+ 20 *item-width*)
-  "Width of toolbar containing buttons, labels and so on.")
-
-(defn- make-padding
-  "Adds a padding to buttons."
-  [buttons]
-  (.add buttons (Box/createRigidArea (Dimension. 0 2))))
-
-(defn- make-separator
-  "Adds a separator to buttons."
-  [buttons]
-  (let [sep (JSeparator. SwingConstants/HORIZONTAL)]
-    (.setMaximumSize sep (Dimension. *item-width* 1))
-    (.add buttons sep)
-    sep))
-
-(defn- make-button
-  "Uniformly creates buttons for lattice editor."
-  [buttons text]
-  (let [button (JButton. text)]
-    (.add buttons button)
-    (.setAlignmentX button Component/CENTER_ALIGNMENT)
-    (.setMaximumSize button (Dimension. *item-width* *item-height*))
-    button))
-
-(defn- make-label
-  "Uniformly creates labels for lattice editor."
-  [buttons text]
-  (let [label (JLabel. text)]
-    (.add buttons label)
-    (.setMaximumSize label (Dimension. *item-width* *item-height*))
-    (.setAlignmentX label Component/CENTER_ALIGNMENT)
-    (.setHorizontalAlignment label SwingConstants/CENTER)
-    label))
-
-(defn- make-labeled-text-field
-  "Uniformly creates a text field for lattice editor."
-  [buttons label text]
-  (let [#^JTextField text-field (JTextField. text),
-	#^JLabel label (JLabel. label),
-	#^JPanel panel (JPanel.)]
-    (doto panel
-      (.add label)
-      (.add text-field)
-      (.setMaximumSize (Dimension. *item-width* *item-height*)))
-    (.setPreferredSize label (Dimension. (* 0.4 *item-width*) (* 0.8 *item-height*)))
-    (.setPreferredSize text-field (Dimension. (* 0.5 *item-width*) (* 0.8 *item-height*)))
-    (.add buttons panel)
-    text-field))
-
-(defn- make-combo-box
-  "Uniformly creates a combo box from the given choices. First item is
-  selected by default."
-  [buttons choices]
-  (let [#^JComboBox combo-box (JComboBox. (into-array String choices))]
-    (doto combo-box
-      (.setMaximumSize (Dimension. *item-width* *item-height*)))
-    (.add buttons combo-box)
-    combo-box))
 
 
 ;;; Technical Helpers
