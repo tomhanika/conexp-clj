a5cb2127485e991e958223974265906364ac5f86
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-07-21 10:16:11 +0200

    Added fuzzy sets
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ src/conexp/contrib/fuzzy/sets.clj
@@ -0,0 +1,83 @@
+;; Copyright (c) Daniel Borchmann. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns conexp.contrib.fuzzy.sets
+  (:use conexp.main))
+
+(ns-doc "Basic definitions for fuzzy sets")
+
+(set! *warn-on-reflection* true)
+
+;;;
+
+(deftype Fuzzy-Set [^clojure.lang.PersistentHashMap hashmap]
+  ;;
+  Object
+  (equals [this other]
+    (boolean (or (identical? this other)
+                 (when (= (class this) (class other))
+                   (let [ohashmap (.hashmap ^Fuzzy-Set other)]
+                     (and (forall [k (keys hashmap)]
+                            (=> (not (zero? (hashmap k)))
+                                (= (hashmap k) (ohashmap k))))
+                          (forall [k (keys ohashmap)]
+                            (=> (not (zero? (ohashmap k)))
+                                (= (hashmap k) (ohashmap k))))))))))
+  (hashCode [this]
+    (hash-combine-hash Fuzzy-Set hashmap))
+  (toString [this]
+    (str hashmap))
+  ;;
+  clojure.lang.ISeq
+  (first [this]
+    (first hashmap))
+  (next [this]
+    (next hashmap))
+  (more [this]
+    (if-let [n (next hashmap)]
+      n
+      ()))
+  (cons [this o]
+    (if (zero? (second o))
+      this
+      (Fuzzy-Set. (conj hashmap o))))
+  (seq [this]
+    (seq hashmap))
+  (count [this]
+    (count hashmap))
+  (empty [this]
+    (Fuzzy-Set. {}))
+  (equiv [this other]
+    (.equals ^Fuzzy-Set this other))
+  ;;
+  clojure.lang.IFn
+  (invoke [this thing]
+    (let [result (hashmap thing)]
+      (or result 0)))
+  (applyTo [this seq]
+    (if (= 1 (count seq))
+      (.applyTo hashmap seq)
+      (illegal-argument "Cannot apply fuzzy sets to non-singleton sequences."))))
+
+(defn make-fuzzy-set
+  "Constructs a fuzzy set from a given hashmap. The values must be
+  numbers between 0 (exclusive) and 1 (inclusive)."
+  [hashmap]
+  (assert (forall [[k v] hashmap]
+            (and (number? v)
+                 (< 0 v)
+                 (<= v 1))))
+  (Fuzzy-Set. hashmap))
+
+(defmethod print-method Fuzzy-Set [set out]
+  (.write ^java.io.Writer out
+          ^String (str "#F" set)))
+
+;;;
+
+nil
