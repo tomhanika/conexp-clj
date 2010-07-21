7d7aa671853ab8e35f14a91f3da82463d088d171
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-07-21 11:36:25 +0200

    Implementing fuzzy factorization
    which does not work by now (at least for Gödel-Logik), because it does
    not terminate.
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/factor_analysis.clj
+++ src/conexp/contrib/factor_analysis.clj
@@ -7,12 +7,13 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns conexp.contrib.factor-analysis
-  (:use conexp.main)
+  (:use conexp.main
+        [conexp.contrib.fuzzy sets logics])
   (:import [java.util HashMap]))
 
 (ns-doc "Implements factorization algorithms for contexts.")
 
-;;;
+;;; Interface
 
 (defmulti factorize-context
   "Factorize context by given method."
@@ -109,191 +110,85 @@
 
 ;;; Many Valued (with t-norms)
 
-(defmulti t-norm
-  "Returns the t-norm and it's residuum corresponding to the name given."
-  {:arglists '[(t-norm-name)]}
-  identity)
-
-(defmethod t-norm :default
-  [norm]
-  (illegal-argument "Norm " (str norm) " is not known."))
-
-(defmethod t-norm :łukasiewicz
-  [_]
-  [(fn [x y] (max 0 (+ x y -1))),
-   (fn [x y] (min 1 (+ 1 (- x) y)))])
-
-(defmethod t-norm :lukasiewicz
-  [_]
-  (t-norm :łukasiewicz))
-
-(defmethod t-norm :gödel
-  [_]
-  [(fn [x y] (min x y)),
-   (fn [x y] y)])
-
-(defmethod t-norm :goedel
-  [_]
-  (t-norm :gödel))
-
-(defmethod t-norm :product
-  [_]
-  [(fn [x y] (* x y)),
-   (fn [x y] (/ y x))])
-
-;;;
-
-(defmacro- define-fuzzy-operator
-  "Defines a fuzzy operator, which throws an
-  UnsupportedOperationException when called. The operator is meant to be
-  rebound."
-  [name arity]
-  `(defn ~name ~(vec (map (fn [_] (gensym)) (range arity)))
-     (unsupported-operation "The operator " (str '~name) " is not rebound.")))
-
-(define-fuzzy-operator f-star 2)
-(define-fuzzy-operator f-impl 2)
-(define-fuzzy-operator f-and 2)
-(define-fuzzy-operator f-or 2)
-(define-fuzzy-operator f-neg 1)
-
-(defmacro with-fuzzy-logic
-  "For the given t-norm norm and the names of the corresponding operators evaluates body in an
-  dynamic environment where the fuzzy logic for norm is in effect."
-  [norm & body]
-  `(let [[x# y#] (t-norm ~norm)]
-     (binding [~'f-star x#,
-               ~'f-impl y#,
-               ~'f-and (fn [x# y#] (f-star x# (f-impl x# y#))),
-               ~'f-or  (fn [x# y#] (f-and (f-impl (f-impl x# y#) y#)
-                                          (f-impl (f-impl y# x#) x#))),
-               ~'f-neg (fn [x#] (f-impl x# 0))]
-       ~@body)))
-
-;;;
-
-(deftype Fuzzy-Set [hashmap]
-  Object
-  (equals [this other]
-    (boolean (or (identical? this other)
-                 (when (= (class this) (class other))
-                   (let [ohashmap (.hashmap ^Fuzzy-Set other)]
-                     (and (forall [k (keys hashmap)]
-                            (=> (not (zero? (hashmap k)))
-                                (= (hashmap k) (ohashmap k))))
-                          (forall [k (keys ohashmap)]
-                            (=> (not (zero? (ohashmap k)))
-                                (= (hashmap k) (ohashmap k))))))))))
-  (hashCode [this]
-    (hash-combine-hash Fuzzy-Set hashmap))
-  (toString [this]
-    (str hashmap))
-  ;;
-  clojure.lang.ISeq
-  (first [this]
-    (first hashmap))
-  (next [this]
-    (next hashmap))
-  (more [this]
-    (if-let [n (next hashmap)]
-      n
-      ()))
-  (cons [this o]
-    (if (zero? (second o))
-      this
-      (Fuzzy-Set. (conj hashmap o))))
-  (seq [this]
-    (seq hashmap))
-  (count [this]
-    (count hashmap))
-  (empty [this]
-    (Fuzzy-Set. {}))
-  (equiv [this other]
-    (.equals ^Fuzzy-Set this other))
-  ;;
-  clojure.lang.IFn
-  (invoke [this thing]
-    (let [result (hashmap thing)]
-      (or result 0)))
-  (applyTo [this seq]
-    (if (= 1 (count seq))
-      (apply hashmap seq)
-      (illegal-argument "Cannot apply fuzzy sets to non-singleton sequences."))))
-
-(defn make-fuzzy-set [map]
-  (Fuzzy-Set. (select-keys map (map (fn [k] (and (< 0 (map k))
-                                                 (<= (map k) 1)))
-                                    (keys map)))))
-
-(defmethod print-method Fuzzy-Set [set out]
-  (.write out (str "#F" set)))
-
-;;;
-
 (defn fuzzy-object-derivation
   "Computes the fuzzy derivation of the fuzzy set C of objects in
   the given context."
   [context C]
   (let [inz (incidence context)]
-    (Fuzzy-Set. (map-by-fn (fn [m]
-                             (reduce (fn [a g]
-                                       (f-and a (f-impl (C g) (inz [g m]))))
-                                     1
-                                     (objects context)))
-                           (attributes context)))))
+    (make-fuzzy-set (map-by-fn (fn [m]
+                                 (reduce (fn [a g]
+                                           (f-and a (f-impl (C g) (inz [g m]))))
+                                         1
+                                         (objects context)))
+                               (attributes context)))))
 
 (defn fuzzy-attribute-derivation
   "Computes the fuzzy derivation of the fuzzy set D of attributes in
   the given context."
   [context D]
   (let [inz (incidence context)]
-    (Fuzzy-Set. (map-by-fn (fn [g]
-                             (reduce (fn [a m]
-                                       (f-and a (f-impl (D m) (inz [g m]))))
-                                     1
-                                     (attributes context)))
-                           (objects context)))))
+    (make-fuzzy-set (map-by-fn (fn [g]
+                                 (reduce (fn [a m]
+                                           (f-and a (f-impl (D m) (inz [g m]))))
+                                         1
+                                         (attributes context)))
+                               (objects context)))))
 
 (defn- fuzzy-oplus-a
-  ""
+  "Helper function to compute D\\oplus_a j."
   [context U D a j]
   (let [D+ (assoc D j a),
         D+down (fuzzy-attribute-derivation context D+),
         D+down-up (fuzzy-object-derivation context D+down)]
+    (println "D+down =" D+down)
+    (println "D+down-up =" D+down-up)
     (set-of [k l] [[k l] U,
                    :when (>= (f-star (D+down k) (D+down-up l))
                              ((incidence context) [k l]))])))
 
-(defn fuzzy-find-factors
-  "Implements factorization with fuzzy logic. Note that you need to
-  rebind all fuzzy operations."
-  [context]
-  (let [inz          (incidence context),
-        find-maximal (fn [U D]
-                       (max-key second
-                                (map (fn [[g m] v]
-                                       [[m v], (count (fuzzy-oplus-a context U D v m))])
-                                     inz)))]
-    (loop [U (set-of [g m] [g (objects context),
-                            m (attributes context),
-                            :when (not (zero? (inz [g m])))]),
-           F #{}]
-      (if (empty? U)
-        F
-        (let [D (loop [D {},
-                       V 0,
-                       [[j a] value] (find-maximal U D)]
-                  (if (> value V)
-                    (recur (assoc D j a)
-                           value
-                           (find-maximal U D))
-                    D)),
-              C (fuzzy-attribute-derivation context D),
-              F (conj F [C,D]),
-              U (set-of [i j] [[i j] U
-                               :when (not (<= (inz [i j])
-                                              (f-star (C i) (D j))))])]
-          (recur U F))))))
+(defn- find-maximal
+  "Find the maximal [m v], where m is an attribute and v in (0,1],
+  such that the cardinality of the set returned by fuzzy-oplus-a is
+  maximal. Returns the pair [[m v], count], where count is the
+  aforementioned cardinality."
+  [context U D]
+  (apply max-key second
+         (map (fn [[[g m] v]]
+                [[m v], (count (fuzzy-oplus-a context U D v m))])
+              (select-keys (incidence context) U))))
+
+(defmethod factorize-context :fuzzy
+  [_ context logic]
+  (with-fuzzy-logic logic
+    (let [inz          (incidence context),
+          find-maximal (partial find-maximal context)]
+      (loop [U (set-of [g m] [g (objects context),
+                              m (attributes context),
+                              :when (not (zero? (inz [g m])))]),
+             F #{}]
+        (println)
+        (println "F =" F)
+        (println "I =" (select-keys inz U))
+        (if (empty? U)
+          F
+          (let [D (loop [D (make-fuzzy-set {}),
+                         V 0,
+                         [[j a] value] (find-maximal U D)]
+                    (println "[j a] =" [j a])
+                    (println "V =" V)
+                    (println "value =" value)
+                    (if (> value V)
+                      (recur (fuzzy-object-derivation context
+                                                      (fuzzy-attribute-derivation context (assoc D j a)))
+                             value
+                             (find-maximal U D))
+                      D)),
+                C (fuzzy-attribute-derivation context D),
+                F (conj F [C,D]),
+                U (set-of [i j] [[i j] U,
+                                 :when (not (<= (inz [i j])
+                                                (f-star (C i) (D j))))])]
+            (recur U F)))))))
 
 ;;;
 
