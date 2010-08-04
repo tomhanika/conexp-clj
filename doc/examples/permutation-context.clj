6798d15d8d6e41924cdc84340ba501a483fcbd43
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-08-04 10:11:31 +0200

    Added permutation-context.clj as example
    Computes the context K as mentioned in GW97, 1.4, example 12, whose
    concept lattice is isomorphic to the lattice of all permutations on a
    finite set.
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ doc/examples/permutation-context.clj
@@ -0,0 +1,51 @@
+;; Copyright: Daniel Borchmann, 2010
+;; This file is in the public domain.
+
+;; Permutations can be ordered such that they form a complete
+;; lattice. This program computes a formal context for a lattice
+;; isomorphic to the lattice of all permutations on a set {1,...,n}.
+;; For this see Ganter-Wille, 1.4, Example 12.
+
+(in-ns 'user)
+(use 'conexp.main)
+
+;;;
+
+(defn context-crossover
+  "For four given context ctx-{1,2,3,4} computes
+
+     ctx-1 | ctx-2
+    -------+-------
+     ctx-3 | ctx-4
+  "
+  [ctx-1 ctx-2 ctx-3 ctx-4]
+  (context-subposition
+   (context-apposition ctx-1 ctx-2)
+   (context-apposition ctx-3 ctx-4)))
+
+(defn L
+  "Computes the context IL as in example 12."
+  [n]
+  (if (<= n 0)
+    (one-context #{1})
+    (let [L_n-1 (L (- n 1))]
+      (context-crossover (make-context (objects L_n-1) (attributes L_n-1) #{})
+                         L_n-1 L_n-1 L_n-1))))
+
+(defn K
+  "Computes the context IK as in examples 12. Its concept lattice is
+  isomorphic to the lattice of all permutation on the set #{1,...,n}."
+  [n]
+  (if (<= n 0)
+    (one-context #{1})
+    (let [K_n-1 (K (- n 1))]
+      (context-crossover K_n-1 K_n-1 K_n-1 (L (- n 1))))))
+
+;; Try it out!
+
+(count (concepts (K 1))) ;-> 1
+(count (concepts (K 5))) ;-> 120
+
+;;;
+
+nil
