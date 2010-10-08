af94f0033fcfca41d49fff43e78e08b486521819
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-10-08 12:08:58 +0200

    Changed bundle.sh to not include dev dependencies
    and some cruel .nfs* files.
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bundle.sh
+++ bundle.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-lein clean && rm -rfv conexp-clj/ conexp-clj.zip && \
-lein deps && lein jar && \
-mkdir -p conexp-clj/lib/ && \
-cp stuff/libs/*.jar conexp-clj/lib/ && cp stuff/libs/*.clj conexp-clj/lib && \
-cp -r bin lib res AUTHORS LICENSE README conexp-clj && \
-mv conexp-clj.jar conexp-clj/lib/conexp-clj-$(cat VERSION)-$(date +%Y%m%d.%H%M%S).jar && \
+CONEXP_CLJ_JAR=conexp-clj-$(cat VERSION)-$(date +%Y%m%d.%H%M%S).jar
+
+lein deps                                                     && \
+rm -rfv conexp-clj/ conexp-clj.zip                            && \
+lein jar $CONEXP_CLJ_JAR                                      && \
+mkdir -p conexp-clj/lib/                                      && \
+cp stuff/libs/*.jar conexp-clj/lib/                           && \
+cp stuff/libs/*.clj conexp-clj/lib                            && \
+cp -r bin res AUTHORS LICENSE README conexp-clj/              && \
+cp lib/*.jar conexp-clj/lib/                                  && \
+mv $CONEXP_CLJ_JAR conexp-clj/lib/                            && \
 zip -r conexp-clj.zip conexp-clj
