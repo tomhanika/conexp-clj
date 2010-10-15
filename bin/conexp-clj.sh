26c80aca5900a12e0503eb958516e65567e6a94c
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-10-15 12:37:09 +0200

    Symbolic links to bin/conexp-clj.sh now work
    thanks to readlink!
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bin/conexp-clj.sh
+++ bin/conexp-clj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONEXP_CLJ_HOME=$(dirname $0)/../
+CONEXP_CLJ_HOME=$(dirname $(readlink -f $0))/../
 CONEXP_CLJ_INIT=${CONEXP_CLJ_HOME}/lib/conexp-clj.clj
 
 java -server -cp ${CONEXP_CLJ_HOME}/lib/\* jline.ConsoleRunner clojure.main -i ${CONEXP_CLJ_INIT} -r $*
