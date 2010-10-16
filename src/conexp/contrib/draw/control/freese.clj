f7e64a8cd7d34224de52194fb0bd72ad48b6d718
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-10-16 21:48:58 +0200

    Refactoring freese layout control
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/draw/control/freese.clj
+++ src/conexp/contrib/draw/control/freese.clj
@@ -14,45 +14,49 @@
         conexp.contrib.draw.scene-layouts
         conexp.contrib.gui.util)
   (:import [javax.swing JButton JSpinner JFrame]
-           [java.awt.event WindowEvent WindowListener]))
+           [java.awt.event WindowEvent WindowAdapter]))
 
 ;;; Freese layout
 
-(defn freese [frame scn buttons]
+(defn freese
+  "Installs Freese Layout control."
+  [frame scn buttons]
   (let [^JButton btn    (make-button buttons "Freese"),
         ^JSpinner spn   (make-spinner buttons 0 (* 2 Math/PI) 0 0.01),
         layout          (freese-layout (lattice (get-layout-from-scene scn))),
         get-value       #(.getValue spn),
         ^JButton rotate (make-button buttons "Rotate"),
-        rotate-thread   (atom nil)]
+        rotate-thread   (atom nil),
+        start-rotate    #(when-not @rotate-thread
+                           (update-layout-of-scene scn (layout (get-value)))
+                           (fit-scene-to-layout scn)
+                           (reset! rotate-thread
+                                   (Thread. (fn []
+                                              (doseq [angle (drop-while (let [y (get-value)]
+                                                                          (fn [x] (<= x y)))
+                                                                        (cycle (range 0 (* 2 Math/PI) 0.05))),
+                                                      :while @rotate-thread]
+                                                (Thread/sleep 50)
+                                                (.setValue spn angle)))))
+                           (.start ^Thread @rotate-thread)),
+        stop-rotate     #(when @rotate-thread
+                           (.stop ^Thread @rotate-thread)
+                           (reset! rotate-thread nil))]
     (with-action-on btn
       (update-layout-of-scene scn (layout (get-value)))
       (fit-scene-to-layout scn))
     (with-change-on spn
       (update-layout-of-scene scn (layout (get-value))))
     (with-action-on rotate
-      (if-not (nil? @rotate-thread)
-        (do
-          (.stop ^Thread @rotate-thread)
-          (reset! rotate-thread nil))
-        (do
-          (update-layout-of-scene scn (layout (get-value)))
-          (fit-scene-to-layout scn)
-          (reset! rotate-thread
-                  (Thread. #(doseq [angle (drop-while (let [y (get-value)]
-                                                        (fn [x] (<= x y)))
-                                                      (cycle (range 0 (* 2 Math/PI) 0.05))),
-                                    :while (and @rotate-thread
-                                                (.isVisible frame))]
-                              (Thread/sleep 50)
-                              (.setValue spn angle))))
-          (.start ^Thread @rotate-thread))))
+      (if @rotate-thread
+        (stop-rotate)
+        (start-rotate)))
     (.addWindowListener ^JFrame frame
-                        (proxy [WindowListener] []
+                        (proxy [WindowAdapter] []
+                          (windowIconified [win-evt]
+                            (stop-rotate))
                           (windowClosed [win-evt]
-                            (when @rotate-thread
-                              (.stop rotate-thread)
-                              (reset! rotate-thread nil)))))))
+                            (stop-rotate))))))
 
 ;;;
 
