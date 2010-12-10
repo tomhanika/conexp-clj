9ad24b107916ccc22faf8b7ead300de35e1ab7f2
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2010-12-10 12:27:43 +0100

    Fixing clojures version
    to work with swank-clojure.
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
   :min-lein-version "1.3.0"
   :description "A ConExp rewrite in clojure"
   :url "http://www.math.tu-dresden.de/~borch/conexp-clj/"
-  :dependencies [[org.clojure/clojure               "1.3.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure               "1.3.0-master-20101025.190211-26"]
                  [org.clojure.contrib/combinatorics "1.3.0-SNAPSHOT"]
                  [org.clojure.contrib/core          "1.3.0-SNAPSHOT"]
                  [org.clojure.contrib/def           "1.3.0-SNAPSHOT"]
