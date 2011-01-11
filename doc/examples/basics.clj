f55f592e43cd10e8a75af862f5d841f7cb583feb
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-01-11 21:05:16 +0100

    Adding examples from Sebastian
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ doc/examples/basics.clj
@@ -0,0 +1,88 @@
+﻿;; Sebastian Böhm, Daniel Borchmann, 2010
+;; This file is in the public domain
+
+;; How to create a context?
+
+;; A context (G,M,I) consists of two sets G and M and an incidence
+;; realtion I between G and M.  G is a set of objects, M is a set of
+;; attributes and gIm can be read as g has the attribute m.  To create
+;; a context you only have to define these sets. There a two options:
+
+;; 1. By using a defined relation:
+(defvar ctx-1 (make-context [1 2 3] [1 2 3] <=))
+
+;; 2. By defining all sets explicitly:
+(defvar ctx-2 (make-context #{1 2 3 4 5 6}
+                            #{1 2 3 4 5 6}
+                            #{[1 1] [1 2] [1 3] [1 5]
+                              [1 6] [2 2] [2 5] [3 3]
+                              [3 6] [4 4] [4 5] [4 6]
+                              [5 5] [6 6]}))
+
+(defvar ctx-3 (make-context-from-matrix 3 3
+                                        [0 0 1
+                                         1 0 1
+                                         1 1 0]))
+
+(defvar ctx-4 (make-context [1 2 3 4 5 6]
+                            [1 2 3 4 5 6]
+                            (fn [x y]
+                              (= 1 (gcd x y)))))
+
+;; This is the ordinale scale for the Chevron
+;;
+;;       5     6
+;;       | \ / |
+;;       |  4  |
+;;       2     3
+;;         \ /
+;;          1
+;;
+
+;; If you want to use a random context you can use rand-context
+(rand-context #{1 2 3} 0.5)
+
+;; The first parameter is the set of objects and attributes and the
+;; second parameter is the probality for the incidence.
+
+
+;; How to compute all formal concepts?
+
+;; First we define a context ctx-1:
+(defvar ctx-1 (make-context-from-matrix ['a 'b 'c 'd 'e 'f]
+					['a 'b 'c 'd 'e 'f]
+					[1 1 1 0 1 1
+					 0 1 0 0 1 0
+					 0 0 1 0 0 1
+					 0 0 0 1 1 1
+					 0 0 0 0 1 0
+					 0 0 0 0 0 1]))
+
+;; This ist the Chevron again, but this time with a instead of 1, b
+;; instead of 2 etc. With ' (quote) you can use symbols in conexp.
+
+;; Now we can compute the arrow-relations (e.g. to see which objects
+;; or attributes are irreducible.)
+(up-arrows ctx-1)
+(down-arrows ctx-1)
+
+;; To get all formal concepts use
+(concepts ctx-1)
+
+;; To see how many formal concepts you get, you can write:
+(count(concepts ctx-1))
+
+;; You can compute the concept lattice via
+(concept-lattice ctx-1) 
+
+;; To see the diagram of the concept-lattice enter first
+(use 'conexp.contrib.draw)
+
+;; and then you can draw the lattice with
+(draw-lattice (concept-lattice ctx-1))
+
+;; You get the stem base with
+(stem-base ctx-1)
+
+;;
+nil
---
+++ doc/examples/contexts.clj
@@ -0,0 +1,101 @@
+;; Sebastian Böhm, Daniel Borchmann, 2010
+;; This file is in the public domain
+
+;; Working with Formal Contexts
+
+;; Now we want to work with a given context. In the following we want
+;; to use this context
+(defvar ctx-2 (make-context-from-matrix [1 2 3]
+                                        [1 2 3 4]
+                                        [1 1 0 0
+                                         1 1 1 0
+                                         0 0 1 1]))
+
+;; First we want to check wether object- or attribute-sets are
+;; clarified. That means that there are no identical colums or rows in
+;; our context.
+(object-clarified? ctx-2) 
+(attribute-clarified? ctx-2)
+
+;; To chech both in one step use
+(clarified? ctx-2)
+
+;; You can clarify the attribute set or the object set or the complete
+;; context with the following functions
+(clarify-attributes ctx-2)
+(clarify-objects ctx-2)
+(clarify-context ctx-2)
+
+;;Because we want to save the resulting context we take
+(defvar ctx-3 (clarify-context ctx-2))
+
+;; The next step ist to reduce our clarified context. First we check
+(reduced? ctx-2)
+
+;; then we reduce, that means all reducible objects and attributes
+;; will be deleted.
+(reduce ctx-2)
+
+;; If our context is clarified (in this case, we did it before) we can
+;; use
+(reduce-clarified-context ctx-3)
+
+;; Now let's take the Chevron from the basics part again as ctx-1.
+;; We can compute all intents and extents via
+(context-extents ctx-1)
+(context-intents ctx-1)
+
+;; Now I want to get all objects, which attribute a and b have in
+;; common
+(attribute-derivation ctx-1 #{'a 'b})
+
+;; Note that the argument is a set. This also works for the objects:
+(object-derivation ctx-1 #{'c 'd 'f})
+
+;; If you want to compute the closure of a given set of objects or
+;; attributes in our context use
+(context-attribute-closure ctx-1 #{'a 'b})
+(context-object-closure ctx-1 #{'a 'b})
+
+;; There a several operations you can do with contexts, e.g. the
+;; apposition. We define two contexts:
+(defvar ctx-1 (make-context #{1 2 3} #{1 2 3} < ))
+(defvar ctx-2 (make-context-from-matrix [1 2 3] ['a 'b 'c 'd] [1 1 0 1
+							       1 0 1 0
+							       0 0 1 1]))
+
+;; The apposition of these two contexts is
+(context-apposition ctx-1 ctx-2)
+
+;; To compute the dual context (and save it as ctx-3) we use
+(defvar ctx-3 (dual-context ctx-2))
+
+;; Now we can build the subposition of context 1 and context 3:
+(context-subposition ctx-1 ctx-3)
+
+;; If you want to invert a given context use
+(invert-context ctx-1)
+
+;; You can create a composition of two contexts with
+(context-composition ctx-1 ctx-2)
+
+;; The union of two contexts is created by
+(context-union ctx-1 ctx-2)
+
+;; Sum to contexts with
+(context-sum ctx-1 ctx-2)
+
+;; To compute the intersection of two contexts use
+(context-intersection ctx-1 ctx-2)
+
+;; The context product goes like this
+(context-product ctx-1 ctx-2)
+
+;; If you want to do a context semiproduct 
+(context-semiprodukt ctx-1 ctx-2)
+
+;; Compute Xia's product 
+(context-xia-product ctx-1 ctx-2)
+
+;;
+nil
