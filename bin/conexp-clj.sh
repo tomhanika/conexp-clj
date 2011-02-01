2a70668930f23d6a6afad0f623097f34e15cec82
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-02-01 21:06:38 +0100

    Surpress clojure's default greeting on startup
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bin/conexp-clj.sh
+++ bin/conexp-clj.sh
@@ -3,4 +3,4 @@
 CONEXP_CLJ_HOME=$(dirname $(readlink -f $0))/../
 CONEXP_CLJ_INIT=${CONEXP_CLJ_HOME}/lib/conexp-clj.clj
 
-java -server -cp ${CONEXP_CLJ_HOME}/lib/\* jline.ConsoleRunner clojure.main -i ${CONEXP_CLJ_INIT} -r $*
+java -server -cp ${CONEXP_CLJ_HOME}/lib/\* jline.ConsoleRunner clojure.main -i ${CONEXP_CLJ_INIT} -e "" -r $*
