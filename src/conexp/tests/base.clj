4e240d9e1b4d1b231e196d09fc58c04d4af97fc0
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-05-25 15:19:29 +0200

    More tests for all-closed-sets-in-family
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/tests/base.clj
+++ src/conexp/tests/base.clj
@@ -81,7 +81,11 @@
 
 (deftest test-all-closed-sets-in-family
   (is (= '(#{2} #{1 2} #{0 2} #{3} #{1 3} #{0 3} #{2 3})
-         (all-closed-sets-in-family #(< (count %) 3) [3 2 0 1] identity #{2}))))
+         (all-closed-sets-in-family #(< (count %) 3) [3 2 0 1] identity #{2})))
+  (is (= '(#{0} #{0 3} #{0 2} #{0 1})
+         (all-closed-sets-in-family #(< (count %) 3) [0 1 2 3] #(conj % 0) #{})))
+  (is (= '(#{0} #{0 3} #{0 2} #{0 1})
+         (all-closed-sets-in-family #(< (count %) 3) [0 1 2 3] #(conj % 0) #{0}))))
 
 (deftest test-next-closed-set
   (are [set next] (= (next-closed-set [3 2 1] identity set) next)
