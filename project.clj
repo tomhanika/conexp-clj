2705afee37cc363a39797aa3cbe95a215b345684
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-07-24 21:35:44 +0200

    Using swank-clojure 1.3.2
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
                  [org.clojure.contrib/set           "1.3.0-SNAPSHOT"]
                  [org.apache.commons/commons-math   "2.1"]
                  [jline                             "0.9.94"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]]
+  :dev-dependencies [[swank-clojure "1.3.2"]]
   :aot [conexp.main
         conexp.contrib.gui
         conexp.contrib.java]
