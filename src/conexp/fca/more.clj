f6aea06faa3e3f42467af7ce284971a25157ed8d
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-09-04 15:16:33 +0200

    Extracting automorphisms from terminal nodes
    but the filtering condition is not correct.
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/fca/more.clj
+++ src/conexp/fca/more.clj
@@ -262,7 +262,7 @@
 
 ;;; Context Automorphisms
 
-;; McKay Algorithm, very simplified
+;;
 
 (defn context-automorphism?
   "Returns true if and only if the pair [alpha beta] is a context automorphism of ctx."
@@ -274,8 +274,12 @@
          (<=> (contains? (incidence ctx) [g m])
               (contains? (incidence ctx) [(alpha g) (beta m)])))))
 
+;; McKay Algorithm, very simplified
+
 (defn- refine-partition
-  ""
+  "This function is part of the implementation of McKay's algorithm.
+
+"
   [ctx, pi]
   (loop [pi pi,
          m  0,
@@ -308,16 +312,22 @@
               (inc k))))))             ;correct?
 
 (defn- split-partition-at
-  ""
+  "This function is part of the implementation of McKay's algorithm.
+
+"
   [pi u]
   (let [[before-u after-u] (split-with #(not (contains? % u)) pi)]
-    (vec (concat before-u [#{u} (disj (first after-u) u)] (rest after-u)))))
+    (vec (concat before-u
+                 [#{u} (disj (first after-u) u)]
+                 (rest after-u)))))
 
 (defn- terminal-nodes
-  ""
+  "This function is part of the implementation of McKay's algorithm.
+
+"
   [ctx pi]
   (if (every? singleton? pi)
-    (list pi)
+    (list (vec (mapcat identity pi)))
     (mapcat #(terminal-nodes ctx (refine-partition ctx (split-partition-at pi %)))
             (first (remove singleton? pi)))))
 
@@ -325,9 +335,35 @@
   "Computes the context automorphisms of ctx as pairs of bijective mappings acting on the objects
   and the attributes of ctx, respectively.  Returns its result as a lazy sequence."
   [ctx]
-  (unsupported-operation))
+  (let [objs (vec (objects ctx)),
+        atts (vec (attributes ctx)),
+        ctx  (make-context (range (count objs))
+                           (range (count objs) (+ (count objs) (count atts)))
+                           (fn [x y]
+                             (incident? ctx (objs x) (atts (- y (count objs)))))),
+        ;;
+        obj-map (into {} (map-indexed vector objs)),
+        rob-map (map-invert obj-map),
+        att-map (into {} (map-indexed #(vector (+ (count objs) %1) %2) atts)),
+        rat-map (map-invert att-map),
+        ;;
+        tn   (terminal-nodes ctx [(objects ctx) (attributes ctx)])]
+    (distinct
+     (for [pi    tn,
+           sigma tn,
+           :when (= (set-of [(pi g) (pi m)] | [g m] (incidence ctx))
+                    (set-of [(sigma g) (sigma m)] | [g m] (incidence ctx)))]
+       (let [pi      (into {} (map-indexed vector pi))
+             sigma-1 (map-invert (into {} (map-indexed vector sigma)))]
+         [(map-by-fn (comp obj-map sigma-1 pi rob-map)
+                     objs),
+          (map-by-fn (comp att-map sigma-1 pi rat-map)
+                     atts)])))))
 
-(defn- terminal-nodes/pruning
+(defn- terminal-nodes-pruning
+  "This function is part of the implementation of McKay's algorithm.
+
+"
   [ctx partition]
   )
 
