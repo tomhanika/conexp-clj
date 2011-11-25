b9c6df399debc8344b1ec88509ac7724fcb1e0b4
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-11-25 22:51:30 +0100

    Update bundle.sh and don't call lein clean anymore
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- bundle.sh
+++ bundle.sh
@@ -4,13 +4,12 @@ CONEXP_CLJ_JAR=conexp-clj-$(cat VERSION)-$(date +%Y%m%d.%H%M%S).jar
 export LEIN_SNAPSHOTS_IN_RELEASE=1
 
 rm -rf conexp-clj/ conexp-clj.zip                             && \
-lein clean                                                    && \
 lein jar                                                      && \
 mv conexp-clj.jar $CONEXP_CLJ_JAR                             && \
 mkdir -p conexp-clj/lib/                                      && \
 cp stuff/libs/*.jar conexp-clj/lib/                           && \
 cp stuff/libs/*.clj conexp-clj/lib                            && \
-cp -r bin res AUTHORS LICENSE README conexp-clj/              && \
+cp -r bin res AUTHORS LICENSE README.md conexp-clj/           && \
 cp lib/*.jar conexp-clj/lib/                                  && \
 mv $CONEXP_CLJ_JAR conexp-clj/lib/                            && \
 zip -r conexp-clj.zip conexp-clj
