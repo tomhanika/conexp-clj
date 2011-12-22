1c7553013cebaf4bd4b54c0cc726753b2b8a0f24
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2011-12-22 10:30:12 +0100

    Move generators to conexp.util.generators
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

---
+++ src/conexp/tests/util/generators.clj
@@ -0,0 +1,24 @@
+;; Copyright (c) Daniel Borchmann. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns conexp.tests.util.generators
+  (:use clojure.test
+        conexp.util.generators))
+
+;;;
+
+(deftest test-generators
+  (is (= (generate-by (fn runner [x]
+                        (doseq [i (range x)]
+                          (yield i)))
+                      10)
+         (list 0 1 2 3 4 5 6 7 8 9))))
+
+;;;
+
+nil
