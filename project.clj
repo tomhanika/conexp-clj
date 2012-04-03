b5d31ad2ffc86721454d8e815800db75b8e41c54
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2012-04-03 18:30:20 +0200

    Move custom jar files to custom maven repository
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- project.clj
+++ project.clj
@@ -12,6 +12,9 @@
   :min-lein-version "1.3.0"
   :description "A ConExp rewrite in clojure"
   :url "http://daniel.kxpq.de/math/conexp-clj/"
+  :repositories {"math" {:url "http://www.math.tu-dresden.de/~borch/repos/mvn/",
+                         :snapshots {:checksum :ignore},
+                         :releases  {:checksum :ignore}}}
   :dependencies [[org.clojure/clojure             "1.3.0"]
                  [org.clojure/tools.cli           "0.1.0"]
                  [org.clojure/core.incubator      "0.1.0"]
