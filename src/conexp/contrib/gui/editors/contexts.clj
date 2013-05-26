4d833c40679edbdf273b19573aa18e6407f38136
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2013-05-26 13:03:07 +0200

    Reimplement menu structure of context editor
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/gui/editors/contexts.clj
+++ src/conexp/contrib/gui/editors/contexts.clj
@@ -9,7 +9,7 @@
 ;; This file has been written by Immanuel Albrecht, with modifications by DB
 
 (ns conexp.contrib.gui.editors.contexts
-  (:use conexp.base
+  (:use [conexp.base :exclude (select)]
         conexp.fca.contexts
         conexp.fca.lattices
         conexp.io
@@ -19,6 +19,7 @@
         conexp.contrib.gui.util
         conexp.contrib.gui.editors.context-editor.context-editor
         conexp.contrib.gui.plugins.base)
+  (:use seesaw.core)
   (:use [clojure.string :only (replace)
                         :rename {replace replace-str}])
   (:import [java.io File]))
@@ -33,8 +34,8 @@
 
 (define-plugin context-editor
   "Context editor plugin."
-  :load-hook   #(load-context-editor %),
-  :unload-hook #(unload-context-editor %))
+  :load-hook   load-context-editor,
+  :unload-hook unload-context-editor)
 
 ;;; The Actions
 
@@ -89,34 +90,44 @@
 
 ;;; The Hooks
 
-(defvar- context-menu
-  {:name "Context",
-   :content [{:name "New Context",
-              :handler #(context-and-go % (make-context (set-of-range 10)
-                                                        (set-of-range 10)
-                                                        #{}))},
-             {:name "Load Context",
-              :handler load-context-and-go},
-             {:name "Random Context",
-              :handler #(context-and-go % (rand-context 5 5 0.4))},
-             {:name "Second Operand Context",
-              :handler second-op-context-and-go},
-             {}
-             {:name "Save Context",
-              :content (vec (map (fn [format]
-                                   {:name (str (replace-str (str format) ":" "")
-                                               " format"),
-                                    :handler (fn [frame]
-                                               (save-context-and-go frame
-                                                                    (fn [ctx path]
-                                                                      (write-context format ctx path))))})
-                                 (list-context-output-formats)))},
-             {:name "Clone Current Context View"
-              :handler clone-context-and-go},
-             {}
-             {:name "Show Concept Lattice",
-              :handler show-lattice-and-go}]}
-  "Menu for context editor.")
+(defn- context-menu
+  "Returns the context-editor menu for a given frame"
+  [frame]
+  (menu :text "Context",
+        :items [(menu-item :text "New Context",
+                           :listen [:action (fn [_]
+                                              (context-and-go frame
+                                                              (make-context (set-of-range 10)
+                                                                            (set-of-range 10)
+                                                                            #{})))]),
+                (menu-item :text "Load Context",
+                           :listen [:action (fn [_]
+                                              (load-context-and-go frame))]),
+                (menu-item :text "Random Context",
+                           :listen [:action (fn [_]
+                                              (context-and-go frame (rand-context 5 5 0.4)))]),
+                (menu-item :text "Second Operand Context",
+                           :listen [:action (fn [_]
+                                              (second-op-context-and-go frame))]),
+                :separator
+                (menu :text "Save Context",
+                      :items (vec (map (fn [format]
+                                         (menu-item :text (str (replace-str (str format) ":" "")
+                                                               " format"),
+                                                    :listen [:action
+                                                             (fn [_]
+                                                               (save-context-and-go
+                                                                frame
+                                                                (fn [ctx path]
+                                                                  (write-context format ctx path))))]))
+                                       (list-context-output-formats)))),
+                (menu-item :text "Clone Current Context View"
+                           :listen [:action (fn [_]
+                                              (clone-context-and-go frame))]),
+                :separator
+                (menu-item :text "Show Concept Lattice",
+                           :listen [:action (fn [_]
+                                              (show-lattice-and-go frame))])]))
 
 (let [menu-hash (atom {})]
 
@@ -124,7 +135,7 @@
     "Loads the context-editor plugin in frame."
     [frame]
     (swap! menu-hash
-           assoc frame (add-menus frame [context-menu])))
+           assoc frame (add-menus frame [(context-menu frame)])))
 
   (defn- unload-context-editor
     "Unloads the context-editor plugin from frame."
