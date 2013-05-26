f0d73fb3cd7d3e529e0d77ef96a8a9fb46b7163b
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2013-05-26 20:27:44 +0200

    Make toolbar scrollable
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/gui/editors/context_editor/context_editor.clj
+++ src/conexp/contrib/gui/editors/context_editor/context_editor.clj
@@ -240,9 +240,9 @@
           ectx           (ref (make-editable-context ctx)),
           bar            (toolbar :orientation :horizontal
                                   :floatable? false)
-          root           (top-bottom-split bar
+          root           (top-bottom-split (scrollable bar)
                                            (get-widget table)
-                                           :divider-location 43)
+                                           :divider-location 65)
           e-ctx          @ectx,
           widget         (context-editor-widget. root table ectx),
           keystroke-fill (KeyStroke/getKeyStroke KeyEvent/VK_SPACE
