fa5b8f4d018f687cab747e3d66b56b23630c6a43
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2013-09-25 16:01:54 +0200

    Use unicode symbol for implicational arrow
    Experimental: symbol might be too small (note that this is actually LONG
    RIGHTWARDS ARROW).
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/fca/implications.clj
+++ src/conexp/fca/implications.clj
@@ -22,7 +22,7 @@
   (hashCode [this]
     (hash-combine-hash Implication premise conclusion))
   (toString [this]
-    (str "(" premise "  ==>  " conclusion ")")))
+    (str "(" premise " ⟶ " conclusion ")")))
 
 (defmulti premise
   "Returns premise of given object."
