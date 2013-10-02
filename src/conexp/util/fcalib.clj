f54cedce117fca3ddd1490abe157f8b29c33eef7
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2013-10-02 17:52:24 +0200

    fcalib conversion functions for formal contexts
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/util/fcalib.clj
+++ src/conexp/util/fcalib.clj
@@ -8,32 +8,70 @@
 
 (ns conexp.util.fcalib
   (:use conexp.base
+        conexp.fca.contexts
         conexp.fca.implications)
-  (:import [de.tudresden.inf.tcs.fcaapi FCAImplication]
-           [de.tudresden.inf.tcs.fcalib Implication]
-           ;; for now, we use conexp-ng's implementation, until the corresponding code is
-           ;; available in fcalib
-           [fcatools.conexpng.model FormalContext]))
+  (:import [de.tudresden.inf.tcs.fcaapi FCAImplication FCAObject ObjectDescription]
+           [de.tudresden.inf.tcs.fcalib Implication FullObject FormalContext ImplicationSet]))
+
+(ns-doc "Functions to convert to and from fcalib")
 
 ;;
 
-(defn to-fcalib-context [conexp-clj-context]
-  (not-yet-implemented))
+(defn stringify-context
+  "Applies «str» to all objects and attributes of the given formal context «ctx» (in
+  conexp-clj format).  Note that if two objects or attributes receive the same string
+  representation this way, they are merged into one."
+  [context]
+  (-> context
+      (rename-attributes str)
+      (rename-objects str)))
 
-(defn from-fcalib-context [^FormalContext fcalib-context]
-  (not-yet-implemented))
+(defn ^FormalContext to-fcalib-context
+  "Converts formal contexts as they are used by conexp-clj to formal contexts as used by fcalib.
 
-(defn to-fcalib-implication [conexp-clj-implication]
+  Note that currently, due to implementation-specific restrictions, only formal contexts
+  having objects and attributes as strings are allowed."
+  [conexp-clj-context]
+  (let [^FormalContext fcalib-context (fcatools.conexpng.model.FormalContext.)]
+    (doseq [m (attributes conexp-clj-context)]
+      (.addAttribute fcalib-context m))
+    (doseq [g (objects conexp-clj-context)]
+      (let [^FCAobject fcalib-g (FullObject. g (oprime conexp-clj-context #{g}))]
+        (.addObject fcalib-context fcalib-g)))
+    fcalib-context))
+
+(defn from-fcalib-context
+  "Converts formal contexts as they are used by fcalib to formal contexts as used by conexp.clj."
+  [^FormalContext fcalib-context]
+  (let [full-objects        (set (.getObjects fcalib-context)),
+        attributes          (set (.getAttributes fcalib-context)),
+        object-descriptions (reduce! (fn [map, ^FCAObject full-object]
+                                       (assoc! map
+                                         (.getIdentifier full-object)
+                                         (.getDescription full-object)))
+                                     {}
+                                     full-objects),
+        objects             (set (keys object-descriptions))]
+    (make-context-nc objects
+                     attributes
+                     (set-of [g m] [g objects
+                                    m attributes
+                                    :when (.containsAttribute ^ObjectDescription (get object-descriptions g)
+                                                              m)]))))
+
+(defn ^FCAImplication to-fcalib-implication
   "Convert implications as used in conexp-clj to implications as used by fcalib."
+  [conexp-clj-implication]
   (Implication. (premise conexp-clj-implication)
                 (conclusion conexp-clj-implication)))
 
-(defn from-fcalib-implication [^FCAImplication fcalib-implication]
+(defn from-fcalib-implication
   "Convert implications as used in fcalib to implications as used in conexp-clj."
+  [^FCAImplication fcalib-implication]
   (make-implication (.getPremise fcalib-implication)
                     (.getConclusion fcalib-implication)))
 
-(defn to-fcalib-implication-set [conexp-clj-set-of-implications]
+(defn ^ImplicationSet to-fcalib-implication-set [conexp-clj-context conexp-clj-set-of-implications]
   (not-yet-implemented))
 
 ;;
