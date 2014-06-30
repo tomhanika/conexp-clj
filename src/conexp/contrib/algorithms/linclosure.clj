e5e4cb925f714abb89412567723f049e319c110a
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2014-06-30 12:53:06 +0200

    Get rid of bad LinClosure implementation
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/algorithms/linclosure.clj
+++ src/conexp/contrib/algorithms/linclosure.clj
@@ -6,53 +6,13 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(ns conexp.contrib.algorithms.linclosure
+(ns conexp.contrib.algorithms.downing-gallier
   (:use [conexp.base :only (difference)]
         [conexp.fca.implications :only (premise conclusion make-implication)]))
 
 ;;;
 
-(defn close-under-implications  ;; this is LinClosure
-  "Computes smallest superset of start being closed under given implications."
-  [implications start]
-  (let [[counts list newdep] (loop [implications implications,
-                                    counts       (transient {}),
-                                    list         (transient {}),
-                                    newdep       (set start)]
-                               (if-let [impl (first implications)]
-                                 (let [impl-count (count (premise impl))]
-                                   (if (zero? impl-count)
-                                     (recur (rest implications)
-                                            counts
-                                            list
-                                            (into newdep (conclusion impl)))
-                                     (recur (rest implications)
-                                            (assoc! counts impl impl-count)
-                                            (reduce (fn [list a]
-                                                      (assoc! list a (conj (get list a) impl)))
-                                                    list
-                                                    (premise impl))
-                                            newdep)))
-                                 [counts list newdep]))]
-    (loop [counts (persistent! counts)
-           update newdep,
-           newdep newdep]
-      (if (empty? update)
-        newdep
-        (let [a (first update),
-              [counts newdep update]
-                (reduce (fn [[counts newdep update] impl]
-                          (let [cnt (dec (get counts impl))]
-                            (if (zero? cnt)
-                              [(assoc counts impl cnt)
-                               (into newdep (conclusion impl))
-                               (into update (difference (conclusion impl) newdep))]
-                              [(assoc counts impl cnt)
-                               newdep
-                               update])))
-                        [counts newdep (disj update a)]
-                        (get list a))]
-          (recur counts update newdep))))))
+
 
 ;;;
 
