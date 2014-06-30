49ddbe492a547b276179eea077520dad46992f27
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2014-06-30 14:09:47 +0200

    First version of Downing-Gallier
    seems to do what it should, but definitively needs more testing
    
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/algorithms/linclosure.clj
+++ src/conexp/contrib/algorithms/linclosure.clj
@@ -7,26 +7,65 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns conexp.contrib.algorithms.downing-gallier
-  (:use [conexp.base :only (difference)]
-        [conexp.fca.implications :only (premise conclusion make-implication)]))
+  (:use [conexp.base :only (not-yet-implemented, def-)]
+        [conexp.fca.implications :only (premise conclusion)]))
 
 ;;;
 
+(defn implication-graph [base-set implications]
+  (let [implications     (vec implications),
+        where-in-premise (reduce (fn [map [i impl]]
+                                   (reduce (fn [map m]
+                                             (update-in map [m] conj i))
+                                           map
+                                           (premise impl)))
+                                 {}
+                                 (map vector (range) implications))
+        numargs          (into {} (map vector
+                                       (range)
+                                       (map (comp count premise) implications)))]
+    [implications where-in-premise numargs]))
 
+(defn close-with-network [base-set implications input-set]
+  (let [[implications in-premise numargs] (implication-graph base-set implications),
+        numargs                           (reduce (fn [numargs i]
+                                                    (update-in numargs [i] dec))
+                                                  numargs
+                                                  (mapcat in-premise input-set))
+        empty-premise-implications        (map first
+                                               (filter (fn [[i c]] (zero? c))
+                                                       numargs))]
+    (loop [queue   (into (clojure.lang.PersistentQueue/EMPTY)
+                         empty-premise-implications),
+           numargs numargs
+           result  input-set]
+      (if (empty? queue)
+        result
+        (let [idx             (first queue),
+              _ (println idx)
+              new             (difference (conclusion (implications idx)) result)
+              [numargs queue] (reduce (fn [[numargs queue] i]
+                                        (let [numargs (update-in numargs [i] dec)]
+                                          [numargs (if (pos? (get numargs i))
+                                                     queue
+                                                     (conj queue i))]))
+                                      [numargs (pop queue)]
+                                      (mapcat in-premise new))
+              _ (println numargs)]
+          (recur queue numargs (into result new)))))))
 
 ;;;
 
-(comment
+(do
 
-  (require '[conexp.main :as cm])
+  (def- testing-implications #{(make-implication '#{p_3 p_4} '#{p_5})
+                               (make-implication '#{p_1} '#{p_2})
+                               (make-implication '#{p_2} '#{p_1})
+                               (make-implication '#{p_3} '#{p_4})
+                               (make-implication '#{}    '#{p_3})})
 
-  (let [subsets-10 (cm/subsets #{1 2 3 4 5 6 7 8 9 10})]
-    (def- impls (cm/set-of (cm/make-implication A B)
-                           [A subsets-10
-                            B subsets-10])))
-
-  (time (close-under-implications impls #{}))
-  (time (cm/close-under-implications impls #{}))
+  (def- network (implication-graph '[p_1 p_2 p_3 p_4 p_5]
+                                   testing-implications))
 
   nil)
 
