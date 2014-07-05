530e64b4a5826d8da3be6e73b8af3388ad2c08e2
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2014-07-05 11:33:40 +0200

    Adjusting tests for conexp.contrib.algorithms
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/tests/algorithms.clj
+++ src/conexp/contrib/tests/algorithms.clj
@@ -7,16 +7,41 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns conexp.contrib.tests.algorithms
-  (:use conexp.main
-        clojure.test)
-  (:require [conexp.contrib.exec :as e]
-            [conexp.contrib.algorithms :as a]
-            [conexp.tests.fca.contexts :as c]))
+  (:use clojure.test)
+  (:require [conexp.main :as cm])
+  (:use [conexp.base :only (def- tests-to-run)]
+        [conexp.contrib.exec :only (program-exists?)]
+        conexp.contrib.algorithms))
 
-;;; Rest
 
-(tests-to-run conexp.contrib.tests.algorithms.concepts
-              conexp.contrib.tests.algorithms.titanic)
+;;;
+
+(defn- add-if-not-exists [invalids program-name keyword]
+  (when-not (program-exists? program-name)
+    (conj invalids keyword)))
+
+(def- concepts-methods (let [invalid-methods (-> #{}
+                                                 (add-if-not-exists "pcbo" :pcbo)
+                                                 (add-if-not-exists "fcbo" :fcbo))]
+                         (remove #(or (.startsWith ^String (name %) "default")
+                                      (contains? invalid-methods %))
+                                 (keys (methods concepts)))))
+
+(def- test-runs 50)
+
+(deftest test-concepts
+  (dotimes [_ test-runs]
+    (let [ctx (cm/rand-context (cm/set-of-range (rand 15)) (rand)),
+          rst (map set (keep #(try (concepts % ctx) (catch Exception _ nil))
+                             concepts-methods))]
+      (if-not (apply = rst)
+        (do (println "concepts returned different result for\n" ctx)
+            (is false))
+        (is true)))))
+
+;;;
+
+(tests-to-run conexp.contrib.tests.algorithms.titanic)
 
 ;;;
 
