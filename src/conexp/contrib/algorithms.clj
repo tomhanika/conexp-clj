c7f23326a55e7b4a6fd80158a053bc08d9dba70f
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2014-07-05 11:12:18 +0200

    Docstring for conexp.contrib.algorithms
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/algorithms.clj
+++ src/conexp/contrib/algorithms.clj
@@ -7,6 +7,7 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns conexp.contrib.algorithms
+  "Provides some optimized versions of the standard algorithms of conexp.main"
   (:require [conexp.main :as cm]
             [clojure.core.reducers :as r]
             conexp.contrib.algorithms.linclosure))
