82f2326a4d2b971e3f0bb4a3caaa9ed6b9b73801
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2014-07-05 14:02:18 +0200

    First fast version of canonical-base
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/conexp/contrib/algorithms/canonical_base.clj
+++ src/conexp/contrib/algorithms/canonical_base.clj
@@ -17,67 +17,40 @@
 ;;; Computing Implicational Closures
 
 (defn- add-immediate-elements
-  [implications initial-set subset-test]
-  (loop [conclusions  (transient initial-set),
+  [implications, ^BitSet initial-set, subset-test]
+  (loop [^BitSet conclusions  initial-set,
          impls        implications,
          unused-impls (transient [])]
     (if-let [^Implication impl (first impls)]
       (if (subset-test (.premise impl) initial-set)
-        (recur (reduce conj! conclusions (.conclusion impl))
+        (recur (do (dobits [x (.conclusion impl)]
+                     (.set conclusions x))
+                   conclusions)
                (rest impls)
                unused-impls)
         (recur conclusions
                (rest impls)
                (conj! unused-impls impl)))
-      [(persistent! conclusions)
-       (persistent! unused-impls)])))
+      [conclusions, (persistent! unused-impls)])))
 
 (defn close-under-implications
-  "Computes smallest superset of set being closed under given implications."
-  [implications set]
-  (assert (set? set))
+  [implications, ^BitSet set]
   (loop [set   set,
          impls implications]
-    (let [[new impls] (add-immediate-elements impls set subset?)]
+    (let [[new impls] (add-immediate-elements impls (.clone set)
+                                              (fn [^BitSet A, ^BitSet B]
+                                                (forall-in-bitset [x A]
+                                                  (.get B x))))]
       (if (= new set)
         new
         (recur new impls)))))
 
 (defn clop-by-implications
-  "Returns closure operator given by implications."
   [implications]
   (partial close-under-implications implications))
 
 
 ;;; Canonical Base
-
-(defn canonical-base-from-clop
-  ([clop base]
-     (canonical-base-from-clop clop base #{}))
-  ([clop base background-knowledge]
-     (assert (fn? clop)
-             "Given closure operator must be a function")
-     (assert (coll? base)
-             "Base must be a collection")
-     (assert (and (set? background-knowledge)
-                  (cm/forall [x background-knowledge]
-                    (implication? x)))
-             "Background knowledge must be a set of implications")
-     (let [next-closure (fn [implications last]
-                          (cm/next-closed-set base
-                                              (clop-by-implications implications)
-                                              last)),
-           runner       (fn runner [implications candidate]
-                          (when candidate
-                            (let [conclusions (clop candidate)]
-                              (if (not= candidate conclusions)
-                                (let [impl  (cm/make-implication candidate conclusions),
-                                      impls (conj implications impl)]
-                                  (cons impl
-                                        (lazy-seq (runner impls (next-closure impls candidate)))))
-                                (recur implications (next-closure implications candidate))))))]
-       (lazy-seq (runner background-knowledge
-                         (close-under-implications background-knowledge #{}))))))
 
 (defn canonical-base
   ([ctx]
@@ -85,9 +58,33 @@
   ([ctx background-knowledge]
      (assert (context? ctx)
              "First argument must be a formal context")
-     (canonical-base-from-clop #(cm/context-attribute-closure ctx %)
-                               (attributes ctx)
-                               background-knowledge)))
+     (with-binary-context ctx 
+       (let [next-closure (fn [implications last]
+                            (next-closed-set object-count
+                                             attribute-count
+                                             (clop-by-implications implications)
+                                             last)),
+             runner       (fn runner [implications, ^BitSet candidate]
+                            (when candidate
+                              (let [conclusions (bitwise-context-attribute-closure
+                                                 object-count
+                                                 attribute-count
+                                                 incidence-matrix
+                                                 candidate)]
+                                (if (not= candidate conclusions)
+                                  (let [impl  (Implication. candidate
+                                                            (filter-bitset #(not (.get candidate %))
+                                                                           conclusions)),
+                                        impls (conj implications impl)]
+                                    (cons impl
+                                          (lazy-seq (runner impls (next-closure impls candidate)))))
+                                  (recur implications (next-closure implications candidate))))))]
+         (map (fn [bit-impl]
+                (cm/make-implication (to-hashset attribute-vector (premise bit-impl))
+                                     (to-hashset attribute-vector (conclusion bit-impl))))
+              (lazy-seq (runner (vec background-knowledge)
+                                (close-under-implications background-knowledge
+                                                          (to-bitset attribute-vector #{})))))))))
 
 ;;;
 
