0c7a7d72712cf14f1a7e32ec9db77eb263bb48e5
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2015-06-29 14:46:02 +0200

    Introduce REPLy
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- src/scripts/conexp-clj.bat
+++ src/scripts/conexp-clj.bat
@@ -3,4 +3,4 @@
 set CONEXP_CLJ_HOME=..
 set CONEXP_CLJ_INIT=%CONEXP_CLJ_HOME%/lib/conexp-clj.clj
 
-java -Dawt.useSystemAAFontSettings=on -cp "%CONEXP_CLJ_HOME%/lib/*" clojure.main -e "" %CONEXP_CLJ_INIT% %*
+java -Dawt.useSystemAAFontSettings=on -cp "%CONEXP_CLJ_HOME%/lib/*" reply.ReplyMain --init %CONEXP_CLJ_INIT% %*
