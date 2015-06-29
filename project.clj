416e51bb42b675700ae1d6dee6588face9c5e15f
Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>
2015-06-29 14:51:03 +0200

    Update version
    Signed-off-by: Daniel Borchmann <daniel.borchmann@mailbox.tu-dresden.de>

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 
 ;;;
 
-(defproject conexp-clj "1.1.0"
+(defproject conexp-clj "1.1.1"
   :min-lein-version "2.0.0"
   :description "A ConExp rewrite in clojure"
   :url "http://github.com/exot/conexp-clj/"
