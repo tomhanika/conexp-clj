e41165ed3f660e65fad19a972b3884dbf2dfcae3
Daniel Borchmann <daniel@algebra20.de>
2016-09-30 13:51:55 +0200

    Remove obsolete ‘extra’ repository
    Signed-off-by: Daniel Borchmann <daniel@algebra20.de>

--- project.clj
+++ project.clj
@@ -14,8 +14,6 @@
   :url "http://github.com/exot/conexp-clj/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["extra" {:url "http://wwwtcs.inf.tu-dresden.de/~borch/repos/mvn/"
-                           :checksum :ignore}]]
   :dependencies [[org.clojure/clojure             "1.8.0"]
                  [org.clojure/tools.cli           "0.3.5"]
                  [org.clojure/math.combinatorics  "0.1.1"]
