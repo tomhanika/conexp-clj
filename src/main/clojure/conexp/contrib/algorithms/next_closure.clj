84c26798c68ffba4d9eb45f647840a70cdf710fe
Daniel Borchmann <daniel@algebra20.de>
2017-03-06 16:01:27 +0100

    Small fix in copyright notice
    Signed-off-by: Daniel Borchmann <daniel@algebra20.de>

--- src/main/clojure/conexp/contrib/algorithms/next_closure.clj
+++ src/main/clojure/conexp/contrib/algorithms/next_closure.clj
@@ -1,4 +1,4 @@
-;; Copyright ⓒ The conexp-clj developers; all rights reserved.
+;; Copyright ⓒ the conexp-clj developers; all rights reserved.
 ;; The use and distribution terms for this software are covered by the
 ;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ;; which can be found in the file LICENSE at the root of this distribution.
