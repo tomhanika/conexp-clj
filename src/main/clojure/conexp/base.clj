300a93647b45eb41c91e4d9b0c3bfb709553e915
Daniel Borchmann <daniel@algebra20.de>
2017-06-24 17:03:49 +0200

    Remove obsolete implementation of general-sorted-sets

--- src/main/clojure/conexp/base.clj
+++ src/main/clojure/conexp/base.clj
@@ -128,8 +128,7 @@ metadata (as provided by def) merged into the metadata of the original."
                conexp.contrib.tests.algorithms.titanic
                conexp.contrib.tests.factor-analysis
                conexp.contrib.tests.fuzzy.sets
-               conexp.contrib.tests.retracts
-               conexp.contrib.tests.util.general-sorted-sets]]
+               conexp.contrib.tests.retracts]]
      (apply require nss)
      (apply clojure.test/run-tests nss))))
 
