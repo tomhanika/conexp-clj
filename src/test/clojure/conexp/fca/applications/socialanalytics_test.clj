3973225287379105ffb46525465e89a552f44bb9
Maximilian Stubbemann <maximilian.stubbemann@gmx.de>
2018-06-27 14:16:55 +0200

    Added tests for socialanalytics.

---
+++ src/test/clojure/conexp/fca/applications/socialanalytics_test.clj
@@ -0,0 +1,157 @@
+;; Copyright ⓒ the conexp-clj developers; all rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns conexp.fca.applications.socialanalytics-test
+  (:require [conexp.fca.contexts :refer [random-context
+                                         random-contexts
+                                         make-context-from-matrix
+                                         objects
+                                         attributes]]
+          [conexp.fca.applications.socialanalytics :refer :all]
+          [clojure.test :refer [deftest is]]
+          [conexp.base :refer [with-testing-data]]))
+
+;Average-shortest-path
+
+(deftest test-average-shortest-path-objects-and-attributes
+  (let [ctx (make-context-from-matrix 4 3
+                          [1 0 1
+                           0 1 0
+                           1 0 0
+                           1 1 0])
+        ctx1 (make-context-from-matrix 3 3
+                                [0 0 0
+                                 0 1 0
+                                 1 0 1])]
+    (is (= (average-shortest-path-objects-and-attributes ctx) (/ 50 21)))
+    (is (= (average-shortest-path-objects-and-attributes ctx1) (/ 5 4)))
+    (is (= (average-shortest-path-objects-and-attributes (random-context 50 0)) nil)))
+  
+  (with-testing-data [ctx (random-contexts 5 50)]
+    (let [value (average-shortest-path-objects-and-attributes ctx)]
+      (or (nil? value)
+          (<= 1
+              value
+              (+ (count (objects ctx))
+                 (count (attributes ctx))))))))
+
+(deftest test-average-shortest-paths-objects
+  (let [ctx (make-context-from-matrix 3 3
+                               [1 0 0
+                                1 1 1
+                                0 0 0])
+        ctx1 (make-context-from-matrix ['a 'b 'c 'd 'e] ['a 'b 'c]
+                                       [1 0 1
+                                        0 1 0
+                                        1 1 0
+                                        0 1 1
+                                        0 0 1])]
+    (is (= (average-shortest-path-objects ctx) 1))
+    (is (= (average-shortest-path-objects ctx1) (/ 13 10)))
+    (is (nil? (average-shortest-path-objects (random-context 50 0)))))
+  
+  (with-testing-data [ctx (random-contexts 5 60)]
+    (let [value (average-shortest-path-objects ctx)]
+      (or (nil? value)
+          (<= 1
+              value
+              (count (objects ctx)))))))
+
+(deftest test-average-shortest-path-attributes
+  (let [ctx (make-context-from-matrix 4 5
+                                      [1 0 1 1 0
+                                       0 1 1 0 0
+                                       1 0 0 1 0
+                                       0 1 1 0 1])
+        ctx1 (make-context-from-matrix 4 4
+                                      [1 0 0 0
+                                       1 1 0 0
+                                       0 1 1 0
+                                       0 0 1 1])]
+    (is (= (average-shortest-path-attributes ctx) (/ 7 5)))
+    (is (= (average-shortest-path-attributes ctx1) (/ 5 3)))
+    (is (nil? (average-shortest-path-attributes (random-context 60 0)))))
+  
+  (with-testing-data [ctx (random-contexts 7 50)]
+    (let [value (average-shortest-path-attributes ctx)]
+          (or (nil? value)
+              (<= 1
+                  value
+                  (count (attributes ctx)))))))
+
+(deftest test-vertice-degrees
+  (let [ctx (make-context-from-matrix 5 3
+                                      [1 1 0
+                                       0 1 0
+                                       0 0 1
+                                       0 1 1
+                                       1 0 1])
+        ctx1 (make-context-from-matrix ['a, 'b, 'c, 'd] ['a, 'b, 'c]
+                                       [1 1 0
+                                        0 1 1
+                                        1 0 0
+                                        0 1 0])]
+    (is (= (sort (vertice-degrees-objects-and-attributes ctx))
+           '(1 1 2 2 2 2 3 3)))
+    (is (= (sort (vertice-degrees-objects-and-attributes ctx1))
+           '(1 1 1 2 2 2 3))))
+  
+  (with-testing-data [ctx (random-contexts 7 150)]
+                     (let [degrees (vertice-degrees-objects-and-attributes ctx)
+                           m (count (objects ctx))
+                           n (count (attributes ctx))]
+                       (and (= (count degrees) (+ m n))
+                            (every? #(<= 0 % (max m n)) degrees)))))
+
+(deftest test-vertice-degrees-objects
+  (let [ctx (make-context-from-matrix 5 3
+                                      [1 1 0
+                                       0 1 0
+                                       0 0 1
+                                       0 1 1
+                                       1 0 1])
+        ctx1 (make-context-from-matrix ['a, 'b, 'c, 'd] ['a, 'b, 'c]
+                                       [1 1 0
+                                        0 1 1
+                                        1 0 0
+                                        0 1 0])]
+    (is (= (sort (vertice-degrees-objects ctx))
+           '(3 3 4 4 5)))
+    (is (= (sort (vertice-degrees-objects ctx1))
+           '(2 3 3 4))))
+  
+  (with-testing-data [ctx (random-contexts 7 150)]
+                     (let [degrees (vertice-degrees-objects ctx)
+                           n (count (objects ctx))]
+                       (and (= (count degrees) n)
+                            (every? #(<= 0 % n) degrees)))))
+
+(deftest test-vertice-degrees-attributes
+  (let [ctx (make-context-from-matrix 5 3
+                                      [1 1 0
+                                       0 1 0
+                                       0 0 1
+                                       0 1 1
+                                       1 0 1])
+        ctx1 (make-context-from-matrix ['a, 'b, 'c] ['a, 'b, 'c, 'd, 'e]
+                                       [1 0 0 1 0
+                                        1 1 1 0 0
+                                        0 0 0 1 0])]
+    (is (= (sort (vertice-degrees-attributes ctx))
+           '(3 3 3)))
+    (is (= (sort (vertice-degrees-attributes ctx1))
+           '(0 2 3 3 4))))
+  
+  (with-testing-data [ctx (random-contexts 7 150)]
+                     (let [degrees (vertice-degrees-attributes ctx)
+                           n (count (attributes ctx))]
+                       (and (= (count degrees) n)
+                            (every? #(<= 0 % n) degrees)))))
+
+;;;
+nil
