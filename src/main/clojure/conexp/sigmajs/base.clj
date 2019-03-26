bc33a794432f782738659e8eae6cbac341d3f98f
Anselm von Wangenheim <anselm.w@gmx.de>
2019-03-26 08:56:51 +0100

    add conexp.sigmajs.base

---
+++ src/main/clojure/conexp/sigmajs/base.clj
@@ -0,0 +1,18 @@
+(ns conexp.sigmajs.base
+  (:require [ubergraph.core :as uber]
+            [loom.graph :as lg]
+            [conexp.fca.lattices :as lat]
+            [conexp.util.graph :exclude [transitive-closure] :refer :all])
+  (:use conexp.base)
+  (:use loom.io))
+
+
+;;;
+
+(defn graph->json
+  )
+
+
+;;;
+
+nil
