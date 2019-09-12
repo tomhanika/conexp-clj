641ff146b22b9fc56a8b4866fada2d3e38becf24
De Narm <s3152806@msx.tu-dresden.de>
2019-09-12 18:52:20 +0200

    API: Added basic dynamic function handling and error messages

--- src/main/clojure/conexp/api/handler.clj
+++ src/main/clojure/conexp/api/handler.clj
@@ -11,23 +11,36 @@
 
 ;;; Macros
 
-(defn read-data [data] nil)
+;;TODO read data macro
+
+(defn read-data [data] data)
+
+;;; Shorthand functions
+
+
 
 ;;; Handler
 
-;;TODO check if valid request by my definition?
-;;TODO read data macro
-;;TODO process-function function
-;;TODO qutomatic sort parameters?
-;;TODO add error messages
+(defn run-function
+  "Given a function object and a collection of data tries to run the 
+  function."
+  [function data]
+  (let [namestring (:name function)
+        id (:id function)
+        args (:args function)]
+   ;; use namestring as function and args as keys in datamap
+   (apply (resolve (symbol namestring)) (map data (map keyword args)))))
 
 (defn process-function 
-  "Tries to either apply the given parameters to the function as is or search
-  for it in a list of pre-defined shorthands. If both fail one of the following
-  error messages is applied:
-  "
+  "Tries to run the function and return a map with the result or error.
+  The status is either 0 on succes or 1 on error."
   [function data]
-  nil)
+  ;; run function an get either a result or error message
+  (let [result (try {:result (run-function function data)}
+                (catch Exception e {:msg (.getMessage e)}))]
+   ;; add an corresponding status to the map
+   (merge (if (contains? result :result) {:status 0} {:status 1})
+          result))) 
 
 (defn handler
   "Handles the JSON request and constructs the JSON response."
@@ -40,8 +53,12 @@
          (let [id (:id (:body request))] (if id (list :id id) (list)))
          ;; tries to parse each object in body besides id and function as data
          ;; afterwards tries to run each function with the data
-         (let [data (read-data (:body request))] 
-          (map #(process-function % data) (:functions (:body request)))))))))
+         (let [body (:body request)
+               data (read-data body)
+               id (:id body)] 
+          (map #(list (keyword (str id (if id "/") (:name %))) 
+                      (process-function % data)) 
+               (:functions body))))))))
 
 ;;;
 
