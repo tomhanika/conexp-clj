82af050cd4809ea879faf7f5e7c0b8e4f5598530
Tom Hanika (sys:companion) <hanika@cs.uni-kassel.de>
2019-11-26 14:59:54 +0100

    Version bump

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(defproject conexp-clj "2.0.2-SNAPSHOT"
+(defproject conexp-clj "2.0.3-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "A ConExp rewrite in clojure"
   :url "http://github.com/tomhanika/conexp-clj/"
