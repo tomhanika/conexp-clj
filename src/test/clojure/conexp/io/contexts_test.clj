bd54feb7d8f394881de06f498426bf0bb79d2da0
De Narm <s3152806@msx.tu-dresden.de>
2020-10-15 22:17:07 +0200

    Added graphml reader
    Added graphml option to docs

--- src/test/clojure/conexp/io/contexts_test.clj
+++ src/test/clojure/conexp/io/contexts_test.clj
@@ -9,6 +9,7 @@
 (ns conexp.io.contexts-test
   (:use conexp.base
         conexp.fca.contexts
+        conexp.fca.many-valued-contexts
         conexp.io.contexts
         conexp.io.util-test)
   (:use clojure.test))
@@ -65,6 +66,61 @@
                       fmt (list-context-formats)]
     (try (out-in-out-in-test ctx 'context fmt)
          (catch UnsupportedOperationException _ true))))
+
+;;; GraphML (seperate testing as it can only be read but not written)
+
+(deftest test-graphml
+  (is (thrown-with-msg? 
+        IllegalArgumentException
+        #"Specified file does not contain valid XML."
+        (read-context "testing-data/Booth.cxt" :graphml)))
+  (is (thrown-with-msg? 
+        IllegalArgumentException
+        #"Specified file not found."
+        (read-context "testing-data/NotExisting.nope" :graphml)))
+  (is (thrown-with-msg? 
+        IllegalArgumentException
+        #"All edges and hyperedges of an hypergraph need ids."
+        (read-context "testing-data/graphml/hyper.graphml" :graphml)))
+  (is (thrown-with-msg? 
+        IllegalArgumentException
+        #"XML file does not contain GraphML."
+        (read-context "testing-data/graphml/fake.graphml" :graphml)))
+  (is (= (first (read-context "testing-data/graphml/simple.graphml" :graphml))
+         (make-context #{"n0" "n1" "n2" "n3" "n4" "n5"} 
+                       #{"n0" "n1" "n2" "n3" "n4" "n5"}
+                       #{["n0" "n2"]["n1" "n2"]["n2" "n3"]
+                         ["n2" "n0"]["n2" "n1"]["n3" "n2"]})))
+  (is (= (first
+           (read-context "testing-data/graphml/hyperids.graphml" :graphml))
+         (make-context #{"n0" "n1" "n2" "n3"} 
+                       #{"1" "3" "4"} 
+                       #{["n0" "1"]["n1" "1"]["n2" "1"]
+                         ["n1" "3"]["n3" "3"]
+                         ["n0" "4"]["n4" "4"]})))
+  (is (= (first
+           (read-context "testing-data/graphml/fuzzy.graphml" :graphml))
+         (make-mv-context #{"n0" "n1" "n2"} 
+                          #{"n0" "n1" "n2"} 
+                          #{["n0" "n1" "1.0"]["n0" "n2" "1.0"]
+                            ["n1" "n2" "2.0"]
+                            ["n1" "n0" "1.0"]["n2" "n0" "1.0"]
+                            ["n2" "n1" "2.0"]})))
+  (is (thrown-with-msg? 
+        IllegalArgumentException
+        #"Multiple data values for edges are not supported."
+        (read-context "testing-data/graphml/fuzzymulti.graphml" :graphml)))
+  (is (thrown-with-msg? 
+        IllegalArgumentException
+        #"Only single values are supported as edge data."
+        (read-context "testing-data/graphml/svg.graphml" :graphml)))
+  (is (= (first
+           (read-context "testing-data/graphml/port.graphml" :graphml))
+         (make-context #{"n0" "n1"} 
+                       #{"0" "1"} 
+                       #{["n0" "0"]["n0" "1"]
+                         ["n1" "0"]["n1" "1"]})))
+  )
 
 ;;;
 
