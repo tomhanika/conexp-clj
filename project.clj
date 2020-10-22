d630fd7f95233babfb2413846dfbd2064e7dbe29
Maximilian Felde <felde@cs.uni-kassel.de>
2020-10-22 11:05:30 +0200

    removed empty lines

--- project.clj
+++ project.clj
@@ -29,14 +29,12 @@
                  [aysylu/loom "1.0.2"]
                  [rolling-stones "1.0.1"
                   :exclusions [org.clojure/clojure]]
-
                  [clj-http "3.10.1"]
                  [clojure-complete "0.2.5"]
                  [ring/ring-devel "1.7.1"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-json "0.5.0"]
                  [http-kit "2.3.0"]
-
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :profiles {:uberjar {:main conexp.main
                        :dependencies [[javax.servlet/servlet-api "2.5"]
