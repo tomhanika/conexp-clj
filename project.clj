36255fe49c9aca0b9496f0281c5da503ee24fc2c
Tom Hanika (sys:companion) <hanika@cs.uni-kassel.de>
2021-01-09 21:55:06 +0100

    Dependencies updated.

--- project.clj
+++ project.clj
@@ -23,19 +23,19 @@
                  [org.clojure/tools.cli           "1.0.194"]
                  [org.apache.commons/commons-math "2.2"]
                  [seesaw                          "1.5.0"]
-                 [reply                           "0.4.3"
+                 [reply                           "0.4.4"
                   :exclusions [org.clojure/clojure
                                clojure-complete
                                com.cemerick/drawbridge]]
                  [aysylu/loom "1.0.2"]
                  [rolling-stones "1.0.1"
                   :exclusions [org.clojure/clojure]]
-                 [clj-http "3.10.1"]
+                 [clj-http "3.11.0"]
                  [clojure-complete "0.2.5"]
-                 [ring/ring-devel "1.7.1"]
-                 [ring/ring-core "1.7.1"]
+                 [ring/ring-devel "1.8.2"]
+                 [ring/ring-core "1.8.2"]
                  [ring/ring-json "0.5.0"]
-                 [http-kit "2.3.0"]
+                 [http-kit "2.5.0"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :profiles {:uberjar {:main conexp.main
                        :dependencies [[javax.servlet/servlet-api "2.5"]
