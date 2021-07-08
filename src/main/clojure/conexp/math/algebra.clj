d626057910bcb90d3d29e7a159f4f54a46c28381
Johannes Hirth <hirth@cs.uni-kassel.de>
2021-07-08 12:23:51 +0200

    Compute all linear extensions

--- src/main/clojure/conexp/math/algebra.clj
+++ src/main/clojure/conexp/math/algebra.clj
@@ -106,6 +106,25 @@
   [set closure]
   (group-by closure set))
 
+;;; Linear Extensions
+(defn all-linear-extensions 
+  "Computes a list of all linear extensions of a Poset O. Also works
+  with concept lattices of conexp.fca.lattices.  The algorithm
+  generates a linear extensions by iteratively adding a minimal
+  elements (according to ord) to the previously generated sequence of
+  elements. All possible extensions by minimal elements are stored in
+  a queue. The algorithm continues until all sequences of the queue
+  are contain all elements of the ordered set."
+  ([ord-set] (all-linear-extensions (base-set ord-set) (order ord-set)))
+  ([elements ord]
+   (loop [les-iter [[[] (set elements)]] les #{}] 
+     (if (empty? les-iter) les
+         (let [[lin non-lin] (first les-iter)]
+           (if (empty? non-lin)
+             (recur (rest les-iter) (conj les lin))
+             (let [pareto-mins (filter (fn [ci] (not (some #(ord % ci) (disj non-lin ci)))) non-lin)
+                   lin+pm (map (fn [ci] [(into lin [ci]) (disj non-lin ci)]) pareto-mins)]
+               (recur (into (rest les-iter) lin+pm) les))))))))
 ;;;
 
 nil
