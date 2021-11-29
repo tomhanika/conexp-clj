307181b32444219c90bfd329ea8b89e8d66fef56
Johannes Hirth <hirth@cs.uni-kassel.de>
2021-11-29 15:02:26 +0100

    More Valuations

--- src/main/clojure/conexp/gui/draw/control/parameters.clj
+++ src/main/clojure/conexp/gui/draw/control/parameters.clj
@@ -1,5 +1,6 @@
 (ns conexp.gui.draw.control.parameters
-  (:require [conexp.fca.metrics :refer [elements-distributivity elements-modularity]]
+  (:require [conexp.fca.metrics :refer :all]
+            [conexp.fca.lattices :refer [extract-context-from-bv]]
             [conexp.gui.draw.control.util :refer :all]
             [conexp.gui.draw.nodes-and-connections :refer :all]
             [conexp.gui.draw.scene-layouts :refer :all]
@@ -17,7 +18,8 @@
 (declare single-move-mode, ideal-move-mode, filter-move-mode, chain-move-mode,
          infimum-additive-move-mode, supremum-additive-move-mode,
          no-valuation-mode, count-int-valuation-mode, count-ext-valuation-mode,
-         modularity-valuation-mode, distributivity-valuation-mode)
+         modularity-valuation-mode, distributivity-valuation-mode,
+         support-valuation-mode, stability-valuation-mode, probability-valuation-mode)
 
 ;;;
 
@@ -70,7 +72,10 @@
                          "count-ints" 'count-int-valuation-mode,
                          "count-exts" 'count-ext-valuation-mode,
                          "distributivity" 'distributivity-valuation-mode,
-                         "modularity" 'modularity-valuation-mode}
+                         "modularity" 'modularity-valuation-mode,
+                         "support" 'support-valuation-mode,
+                         "stability" 'stability-valuation-mode,
+                         "probability" 'probability-valuation-mode}
         ^JComboBox combo-box (make-combo-box buttons (keys valuation-modes)),
         current-valuation-mode (atom (valuation-modes "none"))]
     (listen combo-box :action
@@ -302,6 +307,23 @@
 (defn- distributivity-valuation-mode
   [lat c]
   (float (elements-distributivity lat c)))
+
+(defn- support-valuation-mode
+  [lat c]
+  (float (/ (count (first c))
+            (apply max (map (comp count first) (.base-set lat))))))
+
+(defn- stability-valuation-mode
+  [lat c]
+  (let [ctx (extract-context-from-bv lat)]
+    (float (concept-stability ctx c))))
+
+(defn- probability-valuation-mode
+  [lat c]
+  (let [ctx (extract-context-from-bv lat)]
+    (binding [*fast-computation* true]
+      (if (empty? (second c)) 1
+        (float (concept-probability ctx c)))))) 
 
 (defn- no-valuation-mode
   [_ concept]
