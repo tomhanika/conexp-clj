9f564e3bf4b4d74411bd4f86aeaec71d8523da75
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2023-09-21 18:41:58 +0200

    Cleanup and Documentation.

--- src/main/clojure/conexp/fca/decompositions.clj
+++ src/main/clojure/conexp/fca/decompositions.clj
@@ -1,56 +1,43 @@
+;; Copyright ⓒ the conexp-clj developers; all rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns conexp.fca.decompositions
   (:require [conexp.base :refer :all]
+            [conexp.math.algebra :refer :all]
             [conexp.fca 
              [contexts :refer :all]
              [metrics :refer :all]
              [lattices :refer :all]
-             [posets :refer :all]]
-            ))
+             [posets :refer :all]]))
 
 
-;returns all decompositions pair of "lat" for Libkin decompositions
-(defn libkin-decomposition-pairs [lat]
+(defn libkin-decomposition-pairs 
+  "Returns all decompositions pair of *lat* for Libkin decompositions"
+  [lat]
   (let [neutral-elements (neutral-concepts lat)]
-      (for [n neutral-elements c (element-complement n lat)] [n c])
-))
+      (for [n neutral-elements c (element-complement n lat)] [n c])))
 
 
-(defn lattice-ideal [lat concept]
-  (let [base-set (lattice-base-set lat)
-        order (lattice-order lat)
-        new-base-set (filter #(order % concept) base-set)]
-
-        (make-lattice-nc new-base-set
-                         order
-                         (inf lat)
-                         (sup lat))
-    
-)
-)
+(defn libkin-decomposition-lattices 
+  "Computes the Lattices Resulting from the Libkin-Decomposition on the 
+  Providen Decomposition Pair."
+  [lat decomp-pair]
+  (let [set1 (order-ideal lat (conj #{} (first decomp-pair)))
+        set2 (order-ideal lat (conj #{} (last decomp-pair)))
+        order (lattice-order lat)]
 
 
-(defn libkin-decomposition-lattices [lat decomp-pair]
-  [(lattice-ideal lat (first decomp-pair))
-   (lattice-ideal lat (last decomp-pair))]
-)
+      [(make-lattice-nc set1
+                        order
+                        (inf lat)
+                        (sup lat))
+       (make-lattice-nc set2
+                        order
+                        (inf lat)
+                        (sup lat))]))
 
-
-
-
-;test
-(def ctx (make-context #{1 2 3 4 5} 
-                       #{"A" "B" "C" "D" "E"} 
-                       #{[1 "A"] [2 "A"] [2 "B"] [3 "B"] [3 "C"] [4 "A"] [4 "B"] [4 "C"] [5 "C"] [5 "D"] [5 "E"]}))
-(def lat (concept-lattice ctx))
-
-
-(def ctx2 (make-context #{1 2 3 4 5} 
-                       #{"A" "B" "C" "D" "E"} 
-                       #{[1 "A"] [2 "A"]   [4 "A"] [4 "B"] [4 "C"] [5 "C"] [5 "D"] [5 "E"]}))
-(def lat2 (concept-lattice ctx))
-
-
-(def ctx3 (make-context #{1 2 3 4 5 6 7} #{"A" "B" "C"} #{[1 "A"] [2 "B"] [3 "C"]
-                                                          [4 "A"] [4 "B"] [5 "B"] [5 "C"] [6 "A"] [6 "C"]
-                                                          [7 "A"] [7 "B"] [7 "C"]}))
-(def lat3 (concept-lattice ctx3))
