b80578dc2a45d9c7acd89b8dc2e99e66d297c5e1
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2023-09-27 17:13:39 +0200

    Implemented Product Functions for Posets and Lattices. Improved Tests and Documentation.

--- src/test/clojure/conexp/fca/decompositions_test.clj
+++ src/test/clojure/conexp/fca/decompositions_test.clj
@@ -17,30 +17,62 @@
              [decompositions :refer :all]])
   (:use clojure.test))
 
-
-(deftest test-libkin
-  (let [ctx (make-context #{1 2 3 4 5 6 7} 
+;cube context
+(def ctx1 (make-context #{1 2 3 4 5 6 7} 
                           #{"A" "B" "C"} 
                           #{[1 "A"] [2 "B"] [3 "C"]
                             [4 "A"] [4 "B"] [5 "B"] [5 "C"] [6 "A"] [6 "C"]
-                            [7 "A"] [7 "B"] [7 "C"]})
-        cube-lat (concept-lattice ctx)
-        cube-lat-decomp-pairs (seq [[[#{7 6} #{"C" "A"}] [#{7 4 2 5} #{"B"}]]
+                            [7 "A"] [7 "B"] [7 "C"]}))
+(def lat1 (concept-lattice ctx1))
+
+(def decomp-pairs1 (seq [[[#{7 6} #{"C" "A"}] [#{7 4 2 5} #{"B"}]]
                                     [[#{7 4 2 5} #{"B"}] [#{7 6} #{"C" "A"}]]
                                     [[#{7 1 4 6} #{"A"}] [#{7 5} #{"C" "B"}]]
                                     [[#{7 5} #{"C" "B"}] [#{7 1 4 6} #{"A"}]]
                                     [[#{7 6 3 5} #{"C"}] [#{7 4} #{"B" "A"}]]
                                     [[#{7 1 4 6 3 2 5} #{}] [#{7} #{"C" "B" "A"}]]
                                     [[#{7} #{"C" "B" "A"}] [#{7 1 4 6 3 2 5} #{}]]
-                                    [[#{7 4} #{"B" "A"}] [#{7 6 3 5} #{"C"}]]])
-        decomp-ctx1 (make-context #{6 7}
-                                  #{"A" "B" "C"}
-                                  #{[6 "A"] [6 "C"] [7 "A"] [7 "B"] [7 "C"]})
-        decomp-lat1 (concept-lattice decomp-ctx1)
-        decomp-ctx2 (make-context #{2 4 5 7}
-                                  #{"A" "B" "C"}
-                                  #{[2 "B"] [4 "A"] [4 "B"] [5 "B"] [5 "C"] [7 "A"] [7 "B"] [7 "C"]})
-        decomp-lat2 (concept-lattice decomp-ctx2)]
+                                    [[#{7 4} #{"B" "A"}] [#{7 6 3 5} #{"C"}]]]))
+(def ctx1-decomp-1 (concept-lattice (make-context #{6 7}
+                                                  #{"A" "B" "C"}
+                                                  #{[6 "A"] [6 "C"] [7 "A"] [7 "B"] [7 "C"]})))
+(def ctx1-decomp-2 (concept-lattice (make-context #{2 4 5 7}
+                                                  #{"A" "B" "C"}
+                                                  #{[2 "B"] [4 "A"] [4 "B"] [5 "B"] [5 "C"] [7 "A"] [7 "B"] [7 "C"]})))
 
-      (is (= (libkin-decomposition-pairs cube-lat) cube-lat-decomp-pairs))
-      (is (= (libkin-decomposition-lattices cube-lat [[#{7 6} #{"C" "A"}] [#{7 4 2 5} #{"B"}]]) [decomp-lat1 decomp-lat2]))))
+;no libkin decompositions
+(def ctx2 (make-context #{1 2 3 4 5}
+                        #{1 2 3 4 5}
+                        #{[1 2] [1 3] [1 5]
+                          [2 3]
+                          [3 1] [3 2] [3 3] [3 4]
+                          [4 3] [4 4]
+                          [5 3] [5 5]}))
+(def lat2 (concept-lattice ctx2))
+
+;only decomposable by top and bot
+(def ctx3 (make-context #{1 2 3 4 5 6}
+                        #{1 2 3 4 5 6}
+                        #{[1 1] [1 4] [1 5]
+                          [2 1] [2 5] [2 6]
+                          [3 2] [3 6]
+                          [4 1] [4 5]
+                          [5 2] [5 4]
+                          [6 2] [6 3] [6 4] [6 5]}))
+(def lat3 (concept-lattice ctx3))
+(def decomp-pairs3 (seq [[[#{} #{1 4 6 3 2 5}] [#{1 4 6 3 2 5} #{}]]
+                         [[#{1 4 6 3 2 5} #{}] [#{} #{1 4 6 3 2 5}]]]))
+
+
+(deftest test-libkin
+
+  (is (= (libkin-decomposition-pairs lat1) 
+         decomp-pairs1))
+  (is (= (libkin-decomposition-lattices lat1 [[#{7 6} #{"C" "A"}] [#{7 4 2 5} #{"B"}]]) 
+         [ctx1-decomp-1 ctx1-decomp-2]))
+
+  (is (empty? (libkin-decomposition-pairs lat2)))
+
+  (is (= (libkin-decomposition-pairs lat3) 
+         decomp-pairs3))
+)
