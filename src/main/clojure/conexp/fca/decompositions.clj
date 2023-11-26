8a0b5259fac76958aa968f856cdf0c662ed6a5b2
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2023-11-26 17:15:10 +0100

    Implemented Distributive Exploration Algorithm.

--- src/main/clojure/conexp/fca/decompositions.clj
+++ src/main/clojure/conexp/fca/decompositions.clj
@@ -6,9 +6,10 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(ns conexp.fca.decompositions
+#(ns conexp.fca.decompositions
   (:require [conexp.base :refer :all]
             [conexp.math.algebra :refer :all]
+            [conexp.io.contexts :refer :all]
             [conexp.fca 
              [contexts :refer :all]
              [metrics :refer :all]
@@ -41,3 +42,51 @@
                         (inf lat)
                         (sup lat))]))
 
+(defn add-obj [ctx obj incidence] 
+  "Returns a new context with obj added."
+  (make-context (union (objects ctx) #{obj}) (attributes ctx) incidence))
+
+
+(defn add-attr [ctx attr incidence] 
+  (make-context (objects ctx) (union (attributes ctx) #{attr}) incidence))
+  
+
+
+(defn distributive-exploration [ctx empty-ctx]
+  (let [objs (objects ctx) 
+        attr (attributes ctx)
+        incidence (incidence ctx)
+        start empty-ctx]
+
+      (loop [subctx start
+             rest-objs objs
+             rest-attr attr
+             object-next true]
+
+         (let [new-obj (first (filter #(distributive? (concept-lattice (add-obj subctx % incidence))) rest-objs))
+               new-attr (first (filter #(distributive? (concept-lattice (add-attr subctx % incidence))) rest-attr))]
+
+            (if (not (or new-obj new-attr))
+               subctx
+               (if (and object-next new-obj)
+                  (recur (add-obj subctx new-obj incidence) 
+                         (difference rest-objs #{new-obj})
+                         rest-attr
+                         false)
+               (if (and (not object-next) new-attr)
+                  (recur (add-attr subctx new-attr incidence) 
+                         rest-objs
+                         (difference rest-attr #{new-attr})
+                         true)
+                  (recur subctx
+                         rest-objs
+                         rest-attr
+                         (not object-next)))))
+         )
+      )))
+
+
+
+
+;(def ctx (rad-context "testing-data/Bird-Diet.ctx"))
+;(make-context #{} #{} (incidence ctx))
