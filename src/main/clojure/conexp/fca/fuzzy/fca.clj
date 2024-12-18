4fe40dfec6c352a7a9eae134e308c43726572e62
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2024-12-18 22:26:41 +0100

    Started Reimplementing Functions.

--- src/main/clojure/conexp/fca/fuzzy/fca.clj
+++ src/main/clojure/conexp/fca/fuzzy/fca.clj
@@ -1,4 +1,4 @@
-;; Copyright ⓒ the conexp-clj developers; all rights reserved.
+;; Copyright â the conexp-clj developers; all rights reserved.
 ;; The use and distribution terms for this software are covered by the
 ;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
 ;; which can be found in the file LICENSE at the root of this distribution.
@@ -13,7 +13,6 @@
         conexp.fca.many-valued-contexts
         [conexp.fca.fuzzy sets logics]))
 
-;;;
 
 (deftype Fuzzy-Context [objects attributes incidence]
     Object
@@ -21,7 +20,7 @@
     (generic-equals [this other] Fuzzy-Context [objects attributes incidence]))
   (hashCode [this]
     (hash-combine-hash Fuzzy-Context objects attributes incidence))
-  ;;
+
   conexp.fca.contexts/Context
   (objects [this] objects)
   (attributes [this] attributes)
@@ -124,11 +123,10 @@
                                  (inz [g m]))])]
     (make-context objs atts inci)))
 
-;;;
 
-(defn globalization
-  "Implements globalization."
-  [x]
+
+(defn globalization-hedge [x]
+  "Globalization hedge function."
   (if (= x 1) 1 0))
 
 (defn validity
@@ -143,6 +141,83 @@
                                (fuzzy-aprime fuzzy-context
                                              (make-fuzzy-set A))
                                hedge))))
+
+; Pairs of t-norms and s-norms
+(def lukasiewicz-norm [#(max 0 (+ %1 %2 -1)) 
+                       #(min 1 (+ 1 (- %1) %2))])
+
+(def goedel-norm [#(min %1 %2) 
+                  #(if (<= %1 %2) 1 %2)])
+
+(def product-norm [#(* %1 %2)
+                   #(if (<= %1 %2) 1 (/ %2 %1))])
+
+(defmulti t-norm
+  "Returns the t-norm and it's residuum corresponding to the name given."
+  {:arglists '([(t-norm-name)])}
+  (fn [x & args]
+    (if args
+      (illegal-argument "Wrong number of arguments given.")
+      x)))
+
+(defmethod t-norm :default
+  [norm]
+  (illegal-argument "Norm " (str norm) " is not known."))
+
+(defmethod t-norm :lukasiewicz
+  [_]
+  [(fn [x y] (max 0 (+ x y -1))),
+   (fn [x y] (min 1 (+ 1 (- x) y)))])
+
+;; (defmethod t-norm :łukasiewicz
+;;   [_]
+;;   (t-norm :lukasiewicz))
+
+(defmethod t-norm :goedel
+  [_]
+  [(fn [x y] (min x y)),
+   (fn [x y] (if (<= x y) 1 y))])
+
+;; (defmethod t-norm :gödel
+;;   [_]
+;;   (t-norm :goedel))
+
+(defmethod t-norm :product
+  [_]
+  [(fn [x y] (* x y)),
+   (fn [x y] (if (<= x y) 1 (/ y x)))])
+
+;;; Basic Fuzzy Logic
+
+(defmacro- define-fuzzy-operator
+  "Defines a fuzzy operator, which throws an
+  UnsupportedOperationException when called. The operator is meant to be
+  rebound."
+  [name arity]
+  `(defn ~name ~(vec (map (fn [_] (gensym)) (range arity)))
+     (unsupported-operation "You need to choose a logic with with-fuzzy-logic.")))
+
+(define-fuzzy-operator f-star 2)
+(define-fuzzy-operator f-impl 2)
+(define-fuzzy-operator f-and 2)
+(define-fuzzy-operator f-or 2)
+(define-fuzzy-operator f-neg 1)
+
+(defmacro with-fuzzy-logic
+  "For the given t-norm norm and the names of the corresponding operators evaluates body in an
+  dynamic environment where the fuzzy logic for norm is in effect."
+  [norm & body]
+  `(let [[x# y#] (t-norm ~norm)]
+     (with-altered-vars [~'f-star (constantly x#),
+                         ~'f-impl (constantly y#),
+                         ~'f-and  (constantly
+                                   (fn [x# y#] (f-star x# (f-impl x# y#)))),
+                         ~'f-or   (constantly
+                                   (fn [x# y#] (f-and (f-impl (f-impl x# y#) y#)
+                                                      (f-impl (f-impl y# x#) x#)))),
+                         ~'f-neg  (constantly
+                                   (fn [x#] (f-impl x# 0)))]
+       ~@body)))
 
 ;;;
 
