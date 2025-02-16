9b09b4204cd7d27da7e3bb750f69c250c8fccee5
Tom Hanika (sys:companion) <hanika@cs.uni-kassel.de>
2025-02-16 21:39:23 +0100

    Improved on categorical random contexts

--- project.clj
+++ project.clj
@@ -54,10 +54,10 @@
                    :dependencies [[javax.servlet/servlet-api "2.5"]
                                   [ring/ring-mock "0.4.0"]
                                   [nrepl/nrepl "1.1.2"]
-                                  [com.clojure-goes-fast/clj-async-profiler "1.2.2"]]
+                                  [com.clojure-goes-fast/clj-async-profiler "1.6.1"]]
                    :plugins [[lein-aot-order "0.1.0"]]
                    :javac-options ["-Xlint:deprecation" "-Xlint:unchecked"]
-                   :jvm-opts ["-Djdk.attach.allowAttachSelf"]}}
+                   :jvm-opts ["-Djdk.attach.allowAttachSelf=true"]}}
   :keep-non-project-classes true
   :source-paths ["src/main/clojure" "src/test/clojure"]
   :java-source-paths ["src/main/java"]
